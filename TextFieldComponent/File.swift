2ee684cfd79a0ebc333b97d217153615afcf0e15
Duela Davis <dueladavis123@gmail.com>
2024-09-09 16:02:47 -0400

    updated unscript

---
+++ TextFieldComponent/File.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  TextFieldComponent
+//
+//  Created by Hiran Stephan on 09/09/24.
+//
+
+import Foundation
+
+/* #!/bin/sh
+
+if [ "$YES" = "$OVERRIDE_KOTLIN_BUILD_IDE_SUPPORTED" ]; then
+    echo "Skipping Gradle build task invocation due to OVERRIDE_KOTLIN_BUILD_IDE_SUPPORTED environment variable set to YES"
+    exit 0
+fi
+
+cd $SRCROOT/../
+
+# Embed and sign Apple Framework for Xcode (uncomment if needed)
+# ./gradlew :umbrella:embedAndSignAppleFrameworkForXcode
+
+# Create Swift package
+./gradlew :umbrella:createSwiftPackage
+*/
