273b3eab476091d05b118eb4ced1d1f758d2b3b3
Duela Davis <dueladavis123@gmail.com>
2024-09-10 18:00:15 -0400

    aside

--- TextFieldComponent/File.swift
+++ TextFieldComponent/File.swift
@@ -26,24 +26,48 @@ cd $SRCROOT/../
 /*
  
  public struct LoginGraphicContainer: View {
-     // Access the current horizontal size class of the environment (e.g., Regular for iPad, Compact for iPhone)
+
+     // Optional property for the marketing image
+     private var marketingImage: UIImage?
+
+     /// Access the current horizontal size class of the environment (e.g., Regular for iPad, Compact for iPhone)
      @Environment(\.horizontalSizeClass) var horizontalSizeClass
 
+     // Custom initializer to pass the marketing image
+     public init(marketingImage: UIImage? = nil) {
+         self.marketingImage = marketingImage
+     }
+
      public var body: some View {
-         // VStack contains the placeholder view and dynamically adjusts its size based on the device size class
+         /// Use view builder to decide which view to render
          VStack {
-             // Use the LoginGraphicPlaceholder directly within LoginGraphicContainer
-             loginGraphicPlaceholder()
+             contentView()
+                 .frame(
+                     maxWidth: .infinity,
+                     minHeight: minHeight,  // The minimum height based on the size class (regular or compact)
+                     maxHeight: maxHeight,  // The maximum height based on the size class (regular or compact)
+                     alignment: .center
+                 )
          }
-         .frame(
-             maxWidth: .infinity,      // Ensures the VStack takes up the full width of the screen
-             minHeight: minHeight,     // Dynamically sets the minimum height based on the size class (regular or compact)
-             maxHeight: maxHeight,     // Dynamically sets the maximum height based on the size class (regular or compact)
-             alignment: .center        // Centers the content of the VStack within the available space
-         )
+         .background(BankingTheme.colors.illustrationPink) // Applies a background color to the view
+         .cornerRadius(BankingTheme.dimens.smallMedium)
+     }
+
+     /// ViewBuilder function to return either the marketing image or the login graphic placeholder
+     @ViewBuilder
+     private func contentView() -> some View {
+         if marketingImage == nil {
+             // Placeholder view when marketingImage is nil
+             loginGraphicPlaceholder()
+         } else {
+             // Marketing image is provided, display it using ComponentImage
+             ComponentImage(marketingImage!, resizable: true)
+                 .scaledToFit()
+         }
      }
 
      // Placeholder view logic
+     @ViewBuilder
      private func loginGraphicPlaceholder() -> some View {
          // Check the device's size class to determine layout type
          if horizontalSizeClass == .regular {
@@ -55,8 +79,8 @@ cd $SRCROOT/../
          } else {
              // Vertical layout for iPhone or smaller screens
              VStack(alignment: .center, spacing: BankingTheme.dimens.smallMedium) {
-                 imageView() // Displays the image
-                 textView()  // Displays the title text
+                 imageView()
+                 textView()
              }
          }
      }
@@ -85,20 +109,20 @@ cd $SRCROOT/../
      // Text view component for displaying the title
      private func textView() -> some View {
          Text(Constants.defaultTitle) // Displays the default title text
-             .font(BankingTheme.typography.headingSmall.smallFont) // Sets the font style and size
+             .font(BankingTheme.typography.headingSmall.small.font) // Sets the font style and size
              .foregroundColor(BankingTheme.colors.textPrimary) // Sets the text color
      }
 
      // Constants for the layout and dimensions
      public struct Constants {
-         // Default title to be displayed
+         /// Default title to be displayed
          static let defaultTitle: String = "Sign on to CIBC NetBanking"
 
-         // Maximum width and height for the image
-         static let MaxWidthImage: CGFloat = 135  // Maximum width for the image
+         /// Maximum width and height for the image
+         static let MaxWidthImage: CGFloat = 135 // Maximum width for the image
          static let MaxHeightImage: CGFloat = 67  // Maximum height for the image
 
-         // Adjustable values for min and max heights based on device class (iPad/iPhone)
+         /// Adjustable values for min and max heights based on device class (iPad/iPhone)
          static let MinHeightRegular: CGFloat = 120 // Regular size class (iPad)
          static let MaxHeightRegular: CGFloat = 120
          static let MinHeightCompact: CGFloat = 130 // Compact size class (iPhone)
