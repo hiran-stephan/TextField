87fc72892a9bcfdc433605191212a99dd106260e
Duela Davis <dueladavis123@gmail.com>
2024-09-16 14:08:35 -0400

    sofas

--- TextFieldComponent/ContentView.swift
+++ TextFieldComponent/ContentView.swift
@@ -9,10 +9,17 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .padding()
             
-            // Use the SheetButton to present Sheet One
-            BottomSheetButton(coordinator: coordinator, nextSheet: .sheetOne, title: "Show Sheet One")
-                .bottomSheetManaging(coordinator: coordinator) // Apply the sheet manager to handle sheet presentation
+//            // Use the SheetButton to present Sheet One
+//            BottomSheetButton(coordinator: coordinator, nextSheet: .sheetOne, title: "Show Sheet One")
+//                .bottomSheetManaging(coordinator: coordinator) // Apply the sheet manager to handle sheet presentation
+            Spacer() 
+            SafeSpaceTile {
+                // Action when the button is tapped
+                print("Help button tapped!")
+            }
         }
+        .ignoresSafeArea(.container, edges: .bottom) // Merge the button with the safe area at the bottom
+
     }
 }
 
---
+++ TextFieldComponent/SafeSpaceTile.swift
@@ -0,0 +1,88 @@
+//
+//  HelpButtonView.swift
+//  TextFieldComponent
+//
+//  Created by Hiran Stephan on 16/09/24.
+//
+
+import Foundation
+import SwiftUI
+
+/// A view that displays a help button with customizable height.
+struct SafeSpaceTile: View {
+    
+    /// Action to be performed when the button is tapped.
+    var action: () -> Void
+    
+    /// The maximum height of the button. Default is `Constants.maxHeight`.
+    var maxHeight: CGFloat
+    
+    /// Initializes the help button view.
+    /// - Parameters:
+    ///   - action: The action to be triggered when the button is tapped.
+    ///   - maxHeight: The height of the button. Defaults to `Constants.maxHeight`.
+    init(action: @escaping () -> Void, maxHeight: CGFloat = Constants.maxHeight) {
+        self.action = action
+        self.maxHeight = maxHeight
+    }
+    
+    var body: some View {
+        
+        Button(action: {
+            action() // Perform the provided action when the button is tapped.
+        }) {
+            VStack(alignment: .center, spacing: BankingTheme.spacing.noPadding) {
+                // Chevron icon above the text.
+                ComponentImage(Constants.Images.chevron, resizable: true)
+                    .frame(width: BankingTheme.dimens.large, height: BankingTheme.dimens.large)
+                
+                // Text indicating Need More Help.
+                Text(Constants.textNeedMoreHelp)
+                    .font(BankingTheme.typography.bodySmall.font)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(BankingTheme.colors.textSecondary)
+                    .frame(maxWidth: .infinity, alignment: .top)
+            }
+            .padding(.horizontal, BankingTheme.spacing.noPadding)
+            .padding(.top, BankingTheme.dimens.small)
+            .padding(.bottom, BankingTheme.spacing.noPadding)
+            .frame(maxWidth: .infinity, maxHeight: maxHeight, alignment: .top)
+            .background(BankingTheme.colors.illustrationGrey)
+            .clipShape(RoundedCornersShape(corners: [.topLeft, .topRight], radius: BankingTheme.dimens.smallMedium))
+        }
+        .buttonStyle(PlainButtonStyle())
+        .frame(maxWidth: .infinity)
+    }
+}
+
+/// A shape that rounds specific corners.
+struct RoundedCornersShape: Shape {
+    
+    var corners: UIRectCorner
+    var radius: CGFloat
+    
+    /// Defines the path for the rounded corners.
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+/// Constants used for configuration.
+struct Constants {
+    
+    static let textNeedMoreHelp: String = "Need more help?"
+    static let maxHeight: CGFloat = 68
+    
+    struct Images {
+        static let chevron = "chevron-up"
+    }
+}
+
+
+// Preview
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        HelpButtonView()
+//    }
+//}
