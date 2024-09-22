21830c0977d2b253966d961799f4d5b4534aec7d
Duela Davis <dueladavis123@gmail.com>
2024-09-22 17:13:32 -0400

    asd

--- TextFieldComponent/File.swift
+++ TextFieldComponent/File.swift
@@ -7,60 +7,128 @@
 
 import Foundation
 
-final class LoginFormViewModel: ObservableObject {
-    enum Field {
-        case username
-        case password
-    }
+import SwiftUI
+import Umbrella
+import Components
+import Theme
+import Koin
+
+struct LoginScreen: View {
+    @EnvironmentObject private var theme: Theme
     
-    // MARK: - Published Properties
+    @State private var viewModel: LoginViewModel
+    @StateObject private var form: LoginFormViewModel
+    @ObservedObject private var model: ObservableModel<LoginUIState, LoginActionState>
+    @StateObject var coordinator = BottomSheetCoordinator<ListCellBottomSheet>()
     
-    @Published var editingField: Field?
-    @Published var usernameHint: String = ""
-    @Published var username: String = "" {
-        didSet {
-            if username != oldValue {
-                validationUsername = nil
+    var body: some View {
+        LoadingContentLayout(
+            state: model.state,
+            errorView: {
+                errorContentView
             }
+        ) {
+            contentView
+        }
+        .onAppear {
+            viewModel.attachViewModel(navigator: LoginPageNavigatorImpl())
         }
     }
-    @Published var editingUsername = false
-    @Published var validationUsername: ValidationRule?
     
-    @Published var passwordHint: String = ""
-    @Published var password: String = "" {
-        didSet {
-            if password != oldValue {
-                validationPassword = nil
+    private var contentView: some View {
+        VStack {
+            ScrollView {
+                loginHeaderView
+                
+                if let error = model.action?.error {
+                    errorView(message: error.message)
+                }
+                
+                formContentView
+                    .padding(.top, theme.dimens.large)
+                    .padding(.bottom, theme.dimens.extraLarge)
             }
+            .listRowInsets(EdgeInsets())
+            
+            bottomSafeSpaceTile
+        }
+        .edgesIgnoringSafeArea(.bottom)
+    }
+    
+    private var errorContentView: some View {
+        AnyView(
+            makeResourceErrorView {
+                viewModel.fetchLoginPageContent()
+            }
+        )
+    }
+    
+    private var loginHeaderView: some View {
+        let loginFieldData = LoginFieldDataMapper.map(from: viewModel.createLoginPagePresenter())
+        return VStack(spacing: BankingTheme.dimens.medium) {
+            LoginGraphicContainer(
+                title: loginFieldData.loginGraphicTitle,
+                contentURL: loginFieldData.loginGraphicContentURL
+            )
+            .padding(.horizontal, BankingTheme.dimens.medium)
         }
     }
-    @Published var editingPassword = false
-    @Published var validationPassword: ValidationRule?
     
-    @Published var submitFormData: LoginSubmitFormData?
-    
-    // MARK: - Initialization
-    
-    init(presenter: LoginSubmitFormPresenter) {
-        // Map data from the presenter to the form data model
-        self.submitFormData = LoginSubmitFormDataMapper.map(from: presenter)
+    private var formContentView: some View {
+        VStack(spacing: BankingTheme.dimens.medium) {
+            LoginForm(
+                viewModel: form,
+                isLoading: model.action?.isLoading ?? false,
+                onRecoverUsername: { viewModel.onForgotPasswordClicked() },
+                onForgotPassword: { viewModel.onForgotPasswordClicked() },
+                onSubmitForm: { onSubmitForm() }
+            )
+            .padding(.bottom, theme.dimens.large)
+        }
     }
     
-    // MARK: - Validation Logic
-    
-    func updateValidation(_ result: LoginFormValidationResults) -> Bool {
-        validationUsername = result.username.error
-        validationPassword = result.password.error
-        return result.username.isValid && result.password.isValid
+    private var bottomSafeSpaceTile: some View {
+        VStack {
+            SafeSpaceTile {
+                showSheetOne()
+            }
+            .bottomSheetManaging(coordinator: coordinator)
+        }
     }
     
-    // MARK: - Editing Management
+    init(viewModel: LoginViewModel = KoinApplication.inject()) {
+        self._viewModel = State(initialValue: viewModel)
+        self.model = ObservableModel(
+            statePublisher: asPublisher(viewModel.loginStateWrapped),
+            actionPublisher: asPublisher(viewModel.loginActionWrapped)
+        )
+        // Pass the presenter when initializing the formViewModel
+        _form = StateObject(wrappedValue: LoginFormViewModel(presenter: viewModel.createLoginSubmitFormPresenter()))
+    }
     
-    func edit(field: Field) {
-        editingField = field
-        editingUsername = (field == .username)
-        editingPassword = (field == .password)
+    private func onSubmitForm() {
+        // Validate the form inputs
+        let result = viewModel.validate(username: form.username, password: form.password)
+        if form.updateValidation(result) {
+            viewModel.authenticateCredentials(username: form.username, password: form.password, encrypt: true, encrypted: false)
+        }
+    }
+    
+    private func errorView(message: String?) -> some View {
+        Text(message ?? "Unknown error")
+            .foregroundColor(.red)
+            .multilineTextAlignment(.center)
+            .padding()
+    }
+    
+    private func showSheetOne() {
+        Task {
+            do {
+                await coordinator.transitionToSheet(.sheetOne)
+            } catch {
+                print("Failed to present sheet: \(error)")
+            }
+        }
     }
 }
 
