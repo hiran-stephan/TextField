8b9cd9a120548485196bdbdd98ce010866b2724f
Duela Davis <dueladavis123@gmail.com>
2024-10-29 12:02:28 -0400

    sdf

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,93 +1,94 @@
-//
-//  navigation.swift
-//  TextFieldComponent
-//
-//  Created by Hiran Stephan on 24/10/24.
-//
+
 
 import Foundation
-struct AppNavigation: View {
-    @EnvironmentObject private var viewModel: AppStateViewModel
-    @EnvironmentObject private var navigator: Navigator
+import SwiftUI
+import Theme
+import Koin
+import Umbrella
+
+/// View struct for displaying section details, including a list of items and their actions
+public struct SectionDetailsView: View {
+    // MARK: - Properties
     
-    var body: some View {
-        ZStack {
-            NavigationStackBackport.NavigationStack(path: $navigator.path) {
-                if isHomeNavigation {
-                    makeBottomNavTabBarView(createTabScreen: { tab in
-                        // Use the correct closure expected by makeBottomNavTabBarView
-                        AnyView(HomeNavigation(item: HomeNavigationItems.Main.shared))
-                    })
-                } else {
-                    SplashScene()
-                        .backport.navigationDestination(for: NavigationItem.self) { value in
-                            makeScreen(selectedPath: value)
+    let header: String
+    let sectionData: [ListCellItemData]
+    let onClick: (String) -> Void
+
+    // Optional padding values for customization
+    let paddingTop: CGFloat?
+    let horizontalContainerPadding: CGFloat?
+    let verticalContainerPadding: CGFloat?
+    let trailingListIconPadding: CGFloat?
+    let leadingListIconPadding: CGFloat?
+    let horizontalListSpacing: CGFloat?
+
+    // MARK: - Initializer
+    
+    public init(
+        header: String,
+        sectionData: [ListCellItemData],
+        paddingTop: CGFloat? = nil,
+        horizontalContainerPadding: CGFloat? = nil,
+        verticalContainerPadding: CGFloat? = nil,
+        trailingListIconPadding: CGFloat? = nil,
+        leadingListIconPadding: CGFloat? = nil,
+        horizontalListSpacing: CGFloat? = nil,
+        onClick: @escaping (String) -> Void
+    ) {
+        self.header = header
+        self.sectionData = sectionData
+        self.paddingTop = paddingTop
+        self.horizontalContainerPadding = horizontalContainerPadding
+        self.verticalContainerPadding = verticalContainerPadding
+        self.trailingListIconPadding = trailingListIconPadding
+        self.leadingListIconPadding = leadingListIconPadding
+        self.horizontalListSpacing = horizontalListSpacing
+        self.onClick = onClick
+    }
+    
+    // MARK: - Body
+    
+    public var body: some View {
+        VStack(alignment: .leading, spacing: BankingTheme.spacing.noPadding) {
+            SectionHeaderView(title: header, paddingTop: paddingTop)
+            
+            ListCardContainer(
+                hasBorder: true,
+                isRoundedShape: true,
+                horizontalPadding: horizontalContainerPadding,
+                verticalPadding: verticalContainerPadding
+            ) {
+                ForEach(sectionData, id: \.actionCellId) { listItem in
+                    let isDividerVisible = listItem != sectionData.last
+                    ListCellItemText(
+                        listCellItemData: listItem,
+                        showDivider: isDividerVisible,
+                        dataTextStyle: BankingTheme.typography.body,
+                        trailingIconPadding: trailingListIconPadding,
+                        leadingIconPadding: leadingListIconPadding,
+                        horizontalSpacing: horizontalListSpacing,
+                        onClick: { selectedItem in
+                            onClick(selectedItem.actionPrimaryLabel)
                         }
+                    )
                 }
             }
-
-            if !viewModel.loading.isEmpty {
-                LoadingView()
-            }
         }
+        .padding(.horizontal, horizontalContainerPadding ?? BankingTheme.spacing.noPadding)
     }
+}
+
+// MARK: - ViewBuilders
+
+private extension SectionDetailsView {
     
-    private var isHomeNavigation: Bool {
-        // Check if the path is not empty and attempt to access the first item
-        if !navigator.path.isEmpty {
-            if let firstPathItem = (navigator.path.box as? NavigationPathBackport)?.items.first {
-                return firstPathItem.domain == HomeNavigationItems.Companion.shared.DOMAIN
-            }
-        }
-        return false
-    }
-
-    
-    func makeScreen(selectedPath: NavigationItem) -> AnyView {
-        switch selectedPath.domain {
-        case AuthenticationNavigationItems.Companion.shared.DOMAIN:
-            return AnyView(AuthenticationNavigation(item: selectedPath))
-        case HomeNavigationItems.Companion.shared.DOMAIN:
-            return AnyView(HomeNavigation(item: selectedPath))
-        case AccountDetailsNavigationItems.Companion.shared.DOMAIN:
-            return AnyView(AccountDetailsNavigation(item: selectedPath))
-        case SettingsNavigationItems.Companion.shared.DOMAIN:
-            return AnyView(SettingsNavigation(item: selectedPath))
-        default:
-            return AnyView(Text("None")) // todo generic error screen
-        }
+    /// ViewBuilder function to display a section header
+    @ViewBuilder
+    func SectionHeaderView(title: String, paddingTop: CGFloat? = nil) -> some View {
+        SectionHeadingView(title)
+            .padding(.horizontal, BankingTheme.spacing.noPadding)
+            .padding(.top, paddingTop ?? BankingTheme.spacing.smallMedium)
+            .frame(maxWidth: .infinity, alignment: .topLeading)
     }
 }
 
-
-import SwiftUI
-
-/// A reusable view for displaying a section header with an uppercase title and custom styling.
-struct SectionHeaderView: View {
-    /// The title text to display in the section header.
-    var title: String
-
-    var body: some View {
-        HStack(alignment: .top, spacing: BankingTheme.spacing.noPadding) {
-            // Displays the uppercase title with custom font and color
-            Text(title.uppercased())
-                .font(BankingTheme.typography.allCapsHeading.font)
-                .foregroundColor(BankingTheme.colors.textPrimary)
-                .frame(maxWidth: .infinity, alignment: .topLeading)
-        }
-        .padding(.horizontal, BankingTheme.spacing.noPadding)
-        .padding(.top, BankingTheme.spacing.paddingLg)
-        .padding(.bottom, BankingTheme.dimensions.smallMedium)
-        .frame(maxWidth: .infinity, alignment: .topLeading)
-    }
-}
-
-// MARK: - Preview
-
-struct SectionHeaderView_Previews: PreviewProvider {
-    static var previews: some View {
-        SectionHeaderView(title: "Section Title")
-            .previewLayout(.sizeThatFits)
-            .padding()
-    }
-}
