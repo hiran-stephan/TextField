f6596908a171d1ea850aa5687ff490c1171f6f03
Duela Davis <dueladavis123@gmail.com>
2024-11-07 15:05:55 -0500

    sdf

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -9,3 +9,20 @@ data class AccountDisplayNames(
     @SerialName("display_names")
     val displayNames: Map<String, LocalizedText>
 )
+
+
+override fun formatAccountDisplayName(
+    nickName: String?,
+    productName: String?,
+    productLookupKey: String?
+): String {
+    val localizedContent = ContentFile(products)
+    
+    return when {
+        !nickName.isNullOrBlank() -> nickName
+        !productLookupKey.isNullOrBlank() -> {
+            localizedContent.localized(key = productLookupKey, lang = locale.lang) ?: productName ?: ""
+        }
+        else -> productName ?: ""
+    }
+}
