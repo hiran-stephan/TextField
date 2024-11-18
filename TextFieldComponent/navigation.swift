02bcb53247bb9ce106f583c89c342ef34ed5a58b
Duela Davis <dueladavis123@gmail.com>
2024-11-18 11:05:48 -0500

    pdf

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,35 +1,121 @@
 
 
-
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class AccountDisplayNames(
-    @SerialName("display_names")
-    val displayNames: Map<String, LocalizedText>
-)
-
-
-override fun formatAccountDisplayName(
-    nickName: String?,
-    productName: String?,
-    productLookupKey: String?
-): String {
-    val localizedContent = ContentFile(products)
-    return when {
-        !nickName.isNullOrBlank() -> nickName
-        !productLookupKey.isNullOrBlank() -> {
-            val localizedValue = localizedContent.localized(key = productLookupKey, lang = locale.lang)
-            if (localizedValue == productLookupKey) productName ?: "" else localizedValue
-        }
-        else -> productName ?: ""
+{
+  "display_names": {
+    "DP.C.82001": {
+      "en": "Personal Certificate"
+    },
+    "DP.C.82002": {
+      "en": "IRA Certificate"
+    },
+    "DP.N.82163": {
+      "en": "Employee NOW"
+    },
+    "DP.N.82164": {
+      "en": "CIBC Choice NOW"
+    },
+    "DP.N.82165": {
+      "en": "Complete Checking"
+    },
+    "DP.N.82166": {
+      "en": "CIBC Personal NOW"
+    },
+    "DP.D.82168": {
+      "en": "Select Checking"
+    },
+    "DP.D.82169": {
+      "en": "Student Checking"
+    },
+    "DP.N.82171": {
+      "en": "Health Savings Account"
+    },
+    "DP.D.82172": {
+      "en": "Loyal Checking"
+    },
+    "DP.D.82174": {
+      "en": "Executive Personal Checking"
+    },
+    "DP.M.82175": {
+      "en": "CIBC Personal Money Market"
+    },
+    "DP.M.82180": {
+      "en": "Premier Money Market"
+    },
+    "DP.S.82181": {
+      "en": "CIBC Personal Savings"
+    },
+    "DP.S.82185": {
+      "en": "Personal Investment Reserve"
+    },
+    "DP.S.82186": {
+      "en": "IRA Savings"
+    },
+    "DP.S.82188": {
+      "en": "Holiday Savings Account"
+    },
+    "DP.S.82189": {
+      "en": "Sprinkler Fitter Vacation Club"
+    },
+    "DP.C.82198": {
+      "en": "Personal Jumbo Certificate"
+    },
+    "DP.C.82215": {
+      "en": "Roth IRA Certificate"
+    },
+    "LN.R.83104": {
+      "en": "Revolving Credit"
+    },
+    "LN.R.83107": {
+      "en": "Revolving Credit"
+    },
+    "LN.R.83109": {
+      "en": "Revolving Credit"
+    },
+    "LN.R.83110": {
+      "en": "Revolving Credit"
+    },
+    "LN.M.83116": {
+      "en": "Mortgage"
+    },
+    "LN.M.83118": {
+      "en": "Mortgage"
+    },
+    "LN.M.83123": {
+      "en": "Mortgage"
+    },
+    "LN.M.83124": {
+      "en": "Mortgage"
+    },
+    "LN.M.83125": {
+      "en": "Mortgage"
+    },
+    "LN.I.83132": {
+      "en": "Installment"
+    },
+    "DP.S.84371": {
+      "en": "Roth IRA Savings"
+    },
+    "DP.D.84662": {
+      "en": "EasyPath Access Account"
+    },
+    "LN.R.89079": {
+      "en": "Revolving Credit"
+    },
+    "DP.N.91168": {
+      "en": "CIBC Edge Checking"
+    },
+    "LN.R.91952": {
+      "en": "Revolving Credit"
+    },
+    "DP.D.96641": {
+      "en": "CIBC Bank USA Smart Account"
+    },
+    "DP.S.97185": {
+      "en": "CIBC Agility Savings"
+    },
+    "DP.C.99077": {
+      "en": "CIBC Agility Certificate"
     }
+  }
 }
 
-override fun sortAccounts(accountList: List<HomeAccount>, contentFile: ContentFile, locale: Locale): List<HomeAccount> {
-    return accountList.sortedWith(compareBy(
-        { createDisplayName(it.nickName, it.productName, it.productLookupKey, contentFile, locale) },
-        { it.accountNumber }
-    ))
-}
