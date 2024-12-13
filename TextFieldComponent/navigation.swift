762b80c68bfa22bbda19df3da86129abdb6cc81c
Duela Davis <dueladavis123@gmail.com>
2024-12-13 18:24:25 -0500

    pdf

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,35 +1,64 @@
-/// A SwiftUI view displaying a section of text with a primary and secondary label.
-/// The section dynamically adjusts its size and emits its frame via a preference key.
-struct AccountTextSection: View {
-    /// The primary text to be displayed.
-    let primaryText: String
-    /// The secondary text to be displayed.
-    let secondaryText: String
+/// A SwiftUI view that represents a small card with a gradient background, styled text, and customizable padding.
+public struct MiniCard: View {
+    /// Initializes the `MiniCard` view.
+    public init() {}
 
-    /// The layout of the view, consisting of primary and secondary text stacked vertically.
-    var body: some View {
-        VStack(alignment: .leading, spacing: BankingTheme.spacing.noPadding) {
-            // Primary body text
-            Text(primaryText)
-                .typography(BankingTheme.typography.body)
-                .foregroundColor(BankingTheme.colors.textPrimary)
-                .frame(maxWidth: .infinity, alignment: .topLeading)
+    public var body: some View {
+        HStack(alignment: .center, spacing: BankingTheme.spacing.noPadding) {
+            // Card text
+            Text("DEBIT")
+                .typography(BankingTheme.typography.bodySmallSemiBold)
+                .multilineTextAlignment(.trailing)
+                .foregroundColor(BankingTheme.colors.background)
+                .padding(.trailing, Spacing.miniCardTrailing)
+                .padding(.top, Spacing.miniCardTop)
+                .padding(.bottom, Spacing.miniCardBottom)
+                .frame(width: Spacing.miniCardTextWidth, height: Spacing.miniCardTextHeight, alignment: .trailing)
+                .background(miniCardGradient)
 
-            // Secondary body text
-            Text(secondaryText)
-                .typography(BankingTheme.typography.bodySmall)
-                .foregroundColor(BankingTheme.colors.textSecondary)
-                .frame(maxWidth: .infinity, alignment: .topLeading)
+            // Card background
+                .background(BankingTheme.colors.primary)
+                .cornerRadius(BankingTheme.dimens.microSmall)
+                .overlay(miniCardOverlay)
         }
-        .frame(maxWidth: .infinity, alignment: .leading)
-        .background(
-            GeometryReader { geometry in
-                Color.clear
-                    .preference(
-                        key: ViewFramePreferenceKey.self,
-                        value: geometry.frame(in: .global)
-                    )
-            }
+    }
+
+    /// A view that adds a rounded rectangle overlay to the card.
+    private var miniCardOverlay: some View {
+        RoundedRectangle(cornerRadius: BankingTheme.dimens.microSmall)
+            .inset(by: Spacing.miniCardInset)
+            .stroke(BankingTheme.colors.background, lineWidth: BankingTheme.spacing.stroke)
+    }
+
+    /// A gradient view used as the card's background.
+    private var miniCardGradient: some View {
+        LinearGradient(
+            stops: [
+                Gradient.Stop(color: Colours.miniCardGradientPrimaryColor, location: 0.00),
+                Gradient.Stop(color: Colours.miniCardGradientSecondaryColor, location: 1.00)
+            ],
+            startPoint: UnitPoint(x: 0, y: 1),
+            endPoint: UnitPoint(x: 0.5, y: 0)
         )
+    }
+}
+
+// MARK: - MiniCard Constants
+
+extension MiniCard {
+    /// Constants related to spacing and dimensions for the `MiniCard`.
+    private struct Spacing {
+        static let miniCardTextHeight: CGFloat = 44.0
+        static let miniCardTextWidth: CGFloat = 70.0
+        static let miniCardInset: CGFloat = 0.5
+        static let miniCardTrailing: CGFloat = 5.0
+        static let miniCardTop: CGFloat = 3.0
+        static let miniCardBottom: CGFloat = 21.0
+    }
+
+    /// Constants for colors used in the `MiniCard` gradient.
+    private struct Colours {
+        static let miniCardGradientPrimaryColor = Color(red: 0.02, green: 0.02, blue: 0.02).opacity(0.3)
+        static let miniCardGradientSecondaryColor = Color.black.opacity(0)
     }
 }
