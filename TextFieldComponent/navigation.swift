8e0b3371e0abe8abd4b65fd579911795c0034eab
Duela Davis <dueladavis123@gmail.com>
2024-12-17 11:36:53 -0500

    sdf

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,14 +1,66 @@
-/// A `PreferenceKey` used to measure and share a view's frame information within its container.
-/// It allows child views to pass their `CGRect` frame values up the view hierarchy.
-struct ContainerFramePreferenceKey: PreferenceKey {
-    /// The default value of the preference, initialized to an empty `CGRect`.
-    static var defaultValue: CGRect = .zero
+public enum BadgeIndicatorType: String {
+    case info = "Info"
+    case success = "Success"
+    case error = "Error"
+    case passive = "Passive"
+    case passiveReversed = "PassiveReversed"
+    case promotion = "Promotion"
 
-    /// Combines the current value and the next value for the preference.
-    /// - Parameters:
-    ///   - value: The current value of the preference.
-    ///   - nextValue: A closure that provides the next `CGRect` value.
-    static func reduce(value: inout CGRect, nextValue: () -> CGRect) {
-        value = nextValue()
+    /// Returns the name of the leading image asset associated with the badge type.
+    public var leadingImage: any FunctionalIcon {
+        switch self {
+        case .promotion:
+            return BankingTheme.icons.functional.star
+        default:
+            return BankingTheme.icons.functional.pending
+        }
+    }
+
+    /// Returns the foreground color of the icon associated with the badge type.
+    public var iconForegroundColor: Color {
+        switch self {
+        case .info:
+            return BankingTheme.colors.onInfo
+        case .success:
+            return BankingTheme.colors.onSuccess
+        case .error:
+            return BankingTheme.colors.error
+        case .passive, .passiveReversed:
+            return BankingTheme.colors.textSecondary
+        case .promotion:
+            return BankingTheme.colors.error
+        }
+    }
+
+    /// Returns the text color associated with the badge type.
+    public var textColor: Color {
+        switch self {
+        case .info:
+            return BankingTheme.colors.onInfo
+        case .success:
+            return BankingTheme.colors.onSuccess
+        case .error:
+            return BankingTheme.colors.error
+        case .passive, .passiveReversed:
+            return BankingTheme.colors.textSecondary
+        case .promotion:
+            return BankingTheme.colors.error
+        }
+    }
+
+    /// Returns the background color associated with the badge type.
+    public var backgroundColor: Color {
+        switch self {
+        case .info:
+            return BankingTheme.colors.illustrationBlueShadow
+        case .success:
+            return BankingTheme.colors.success
+        case .error:
+            return BankingTheme.colors.errorContainer
+        case .passive:
+            return BankingTheme.colors.illustrationGrey
+        case .passiveReversed, .promotion:
+            return BankingTheme.colors.background
+        }
     }
 }
