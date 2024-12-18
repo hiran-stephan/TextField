0e170ab427d5be1c318bda15d95c65014bc1ff4b
Duela Davis <dueladavis123@gmail.com>
2024-12-18 13:19:34 -0500

    pdf

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,27 +1,9 @@
 @ViewBuilder
-private func errorInlineView() -> AnyView {
-    let stateErrorPresenter = viewModel.createErrorPresenter(error: model.state?.error)
-    return AnyView(
-        ErrorInlineListView(
-            alertType: AlertType.failure.rawValue,
-            alertMessage: stateErrorPresenter.formatErrorForGlobalAlertMessage(),
-            alertCode: stateErrorPresenter.formatErrorForGlobalAlertCode()
-        )
-    )
-}
-
-
-@ViewBuilder
-private func errorFullView() -> AnyView {
-    let fullPagePresenter = viewModel.createProblemsFullPagePresenter(stateError: model.state?.error, resourceError: nil)
-    return AnyView(
-        ErrorContentView(
-            imageName: ComponentConstants.Images.alertFullPage,
-            title: fullPagePresenter.formatFullPageErrorTitle(),
-            subtitle: fullPagePresenter.formatErrorForPageCode(),
-            message: fullPagePresenter.formatFullPageAlertMessage(),
-            errorCode: fullPagePresenter.code,
-            actionLabel: fullPagePresenter.formatFullPageErrorCTA()
-        )
-    )
+private func accountList(for accounts: [Account]) -> some View {
+    VStack(alignment: .leading, spacing: BankingTheme.spacing.small) { // Adjust spacing here
+        ForEach(accounts, id: \.self) { account in
+            let presenter = viewModel.createAccountPresenter(account: account)
+            AccountPreferenceCard(data: presenter.toAccountPreferenceCardData())
+        }
+    }
 }
