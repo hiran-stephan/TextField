53209859d65784ba61b5b94e31d9a229a3cf4b28
Duela Davis <dueladavis123@gmail.com>
2024-12-20 11:59:17 -0500

    sdf

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,53 +1,33 @@
-//
-//  AccountPreferencesDetailsScreen.swift
-//  Created by Stephan, Hiran on 2024-12-19.
-//
-
-import SwiftUI
-import Umbrella
-import Theme
-import Components
-import Koin
-
-/// A SwiftUI view that displays the details of account preferences.
-struct AccountPreferencesDetailsScreen: View {
-    // MARK: - Properties
-    
-    /// The view model for managing the account preferences details.
-    @State private var viewModel: AccountPreferencesDetailsViewModel
-
-    /// An observed object to handle state updates for resource and UI states.
-    @ObservedObject private var model: ObservableModelState<AccountPreferencesDetailsResourceUiState, AccountPreferencesDetailsUiState>
-
-    // MARK: - Initializer
-    
-    /// Initializes the screen with the provided view model.
-    /// - Parameter viewModel: The view model for this screen, injected by default.
-    init(viewModel: AccountPreferencesDetailsViewModel) {
-        self._viewModel = State(initialValue: viewModel)
-        self._model = ObservedObject(
-            initialValue: ObservableModelState(
-                resourcePublisher: asPublisher(viewModel.resourceStateWrapped),
-                statePublisher: asPublisher(viewModel.uiStateWrapped)
-            )
-        )
+@ViewBuilder
+private func contentView() -> some View {
+    guard let accountGroups = model.state?.accountsSummary?.accountGroups else {
+        return EmptyView()
     }
-
-    // MARK: - Body
     
-    var body: some View {
-        VStack {
-            // Placeholder for the screen content.
-            Text("AccountPreferencesDetailsScreen")
-            
-            Spacer()
+    VStack(alignment: .leading, spacing: BankingTheme.spacing.noPadding) {
+        ForEach(accountGroups, id: \.self) { accountGroup in
+            let groupPresenter = viewModel.createAccountGroupPresenter(accountGroup: accountGroup)
+            let groupAccounts = groupPresenter.sortedAccountsList()
+            GroupSectionContainerView(groupPresenter: groupPresenter) {
+                ForEach(groupAccounts, id: \.self) { account in
+                    makeAccountRow(account)
+                }
+            }
         }
+        
+        makeLastOnlineView()
+        
+        // Apply padding to the entire VStack
+        Spacer() // Add spacing if needed
     }
+    .padding(.horizontal, BankingTheme.dimens.medium)
+    .padding(.bottom, BankingTheme.dimens.medium)
+    
+    VStack(alignment: .leading, spacing: BankingTheme.spacing.noPadding) {
+        // Other content
+        makeBottomSection()
+    }
+    
 }
 
-// MARK: - Preview
 
-/// A preview for the `AccountPreferencesDetailsScreen`.
-#Preview {
-    AccountPreferencesDetailsScreen(viewModel: AccountPreferencesDetailsViewModel())
-}
