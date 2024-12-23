440e6c6b58670df6e0bf6721c0d8603fb3570793
Duela Davis <dueladavis123@gmail.com>
2024-12-23 12:46:24 -0500

    pdf

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,56 +1,26 @@
-extension AccountPreferencesDetailsScreen {
-    /// A view that displays an account nickname with a button.
-    ///
-    /// This view is used to show a label (account nickname) and a button for further actions.
-    /// The button can trigger a specific function passed as a closure.
-    private struct AccountNicknameView: View {
-        /// The label to display as the account nickname.
-        let label: String
+data object AccountPreferences : SettingsNavigationItems(path = "account-preferences") {
+        override val route: String
+            get() = "/$domain/$path"
 
-        /// The text to display on the button.
-        let buttonText: String
+        override val deepLink: String
+            get() = "/$domain/$path"
 
-        /// The action to perform when the button is tapped.
-        let onButtonTap: () -> Void
-
-        /// Initializes the `AccountNicknameView`.
-        ///
-        /// - Parameters:
-        ///   - label: The label to display as the account nickname.
-        ///   - buttonText: The text to display on the button.
-        ///   - onButtonTap: The closure to execute when the button is tapped.
-        init(label: String, buttonText: String, onButtonTap: @escaping () -> Void) {
-            self.label = label
-            self.buttonText = buttonText
-            self.onButtonTap = onButtonTap
-        }
-
-        var body: some View {
-            HStack(alignment: .top, spacing: BankingTheme.dimensions.mediumLarge) {
-                VStack(alignment: .leading, spacing: BankingTheme.spacing.noPadding) {
-                    Text(label)
-                        .typography(BankingTheme.typography.bodySmall)
-                        .foregroundColor(BankingTheme.colors.textSecondary)
-                        .frame(maxWidth: .infinity, alignment: .topLeading)
-                }
-
-                HStack(alignment: .center, spacing: BankingTheme.dimensions.small) {
-                    TextLinkButton(
-                        title: buttonText,
-                        type: .default,
-                        removePadding: true
-                    ) {
-                        onButtonTap()
-                    }
-                }
-                .padding(.vertical, BankingTheme.dimensions.small)
-                .frame(maxWidth: .infinity, alignment: .leading)
+        companion object {
+            fun parse(url: String): AccountPreferencesDetails {
+                val params = url.parseQueryString()
+                return AccountPreferencesDetails(
+                    id = params["id"]?.firstOrNull() ?: "missing"
+                )
             }
-            .padding(.vertical, BankingTheme.dimensions.mediumLarge)
-            .padding(.horizontal, BankingTheme.dimensions.mediumLarge)
-            .frame(maxWidth: .infinity, alignment: .topLeading)
-            .background(BankingTheme.colors.illustrationGrey)
-            .cornerRadius(BankingTheme.dimensions.smallMedium)
         }
     }
-}
+
+    data class AccountPreferencesDetails(
+        val id: String
+    ) : SettingsNavigationItems(path = "account-preferences/details") {
+        override val route: String
+            get() = "/$domain/$path?id=$id"
+
+        override val deepLink: String
+            get() = "/$domain/$path?id=$id"
+    }
