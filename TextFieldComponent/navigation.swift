25046f3a42074ca56e33dfc89d627bac7c5464ab
Duela Davis <dueladavis123@gmail.com>
2024-12-23 16:03:31 -0500

    sdf

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,26 +1,30 @@
-data object AccountPreferences : SettingsNavigationItems(path = "account-preferences") {
-        override val route: String
-            get() = "/$domain/$path"
+data object AccountPreferences : SettingsNavigationItems(
+    path = "account-preferences"
+) {
+    override val route: String
+        get() = "/$domain/$path"
 
-        override val deepLink: String
-            get() = "/$domain/$path"
+    override val deepLink: String
+        get() = "/$domain/$path"
+}
 
-        companion object {
-            fun parse(url: String): AccountPreferencesDetails {
-                val params = url.parseQueryString()
-                return AccountPreferencesDetails(
-                    id = params["id"]?.firstOrNull() ?: "missing"
-                )
-            }
+
+
+data class AccountPreferencesDetails(
+    val id: String
+) : SettingsNavigationItems(path = "account-preferences/{id}") {
+    override val route: String
+        get() = "/$domain/$path".replace("{id}", id)
+
+    override val deepLink: String
+        get() = "/$domain/$path".replace("{id}", id)
+
+    companion object {
+        fun parse(url: String): AccountPreferencesDetails {
+            val params = url.parseQueryString()
+            return AccountPreferencesDetails(
+                id = params["id"]?.firstOrNull() ?: "missing"
+            )
         }
     }
-
-    data class AccountPreferencesDetails(
-        val id: String
-    ) : SettingsNavigationItems(path = "account-preferences/details") {
-        override val route: String
-            get() = "/$domain/$path?id=$id"
-
-        override val deepLink: String
-            get() = "/$domain/$path?id=$id"
-    }
+}
