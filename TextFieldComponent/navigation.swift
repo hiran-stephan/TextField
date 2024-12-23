d70da7c2473dfcc901b96e89ab0c80a0fac89cd0
Duela Davis <dueladavis123@gmail.com>
2024-12-23 16:30:14 -0500

    sdf

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,30 +1,46 @@
-data object AccountPreferences : SettingsNavigationItems(
-    path = "account-preferences"
-) {
-    override val route: String
-        get() = "/$domain/$path"
+import SwiftUI
+import Umbrella
+import CustomizeBankingUI
+import BiometricsSetupUI
+import CustomerServiceUI
+import SecurityCenterUI
+import AccountPreferencesUI
 
-    override val deepLink: String
-        get() = "/$domain/$path"
-}
+public struct SettingsNavigation: View {
+    private let item: NavigationItem
 
+    public init(item: NavigationItem) {
+        self.item = item
+    }
 
-
-data class AccountPreferencesDetails(
-    val id: String
-) : SettingsNavigationItems(path = "account-preferences/{id}") {
-    override val route: String
-        get() = "/$domain/$path".replace("{id}", id)
-
-    override val deepLink: String
-        get() = "/$domain/$path".replace("{id}", id)
-
-    companion object {
-        fun parse(url: String): AccountPreferencesDetails {
-            val params = url.parseQueryString()
-            return AccountPreferencesDetails(
-                id = params["id"]?.firstOrNull() ?: "missing"
-            )
+    public var body: some View {
+        switch item.route {
+        case SettingsNavigationItems.Main.shared.route:
+            Text("TBD") // Placeholder for Main Scene
+        case SettingsNavigationItems.Customize.shared.route:
+            CustomizeBankingScene()
+                .navigationBarBackButtonHidden()
+        case SettingsNavigationItems.BiometricsSetup.shared.route:
+            BiometricsSetupInstructionsScene()
+                .navigationBarBackButtonHidden()
+        case SettingsNavigationItems.BiometricsSetupPassword.shared.route:
+            BiometricsSetupPasswordScene()
+                .navigationBarBackButtonHidden()
+        case SettingsNavigationItems.SecurityCenterSetup.shared.route:
+            SecurityCenterScene()
+                .navigationBarBackButtonHidden()
+        case SettingsNavigationItems.AccountPreferences.shared.route:
+            AccountPreferencesScene()
+                .navigationBarBackButtonHidden()
+        case let route where route.starts(with: SettingsNavigationItems.AccountPreferencesDetails.shared.route):
+            if let detailsItem = item as? SettingsNavigationItems.AccountPreferencesDetails {
+                AccountPreferencesDetailsScene(navigationItem: detailsItem)
+                    .navigationBarBackButtonHidden()
+            } else {
+                Text("Invalid Details Route")
+            }
+        default:
+            Text("No scene found")
         }
     }
 }
