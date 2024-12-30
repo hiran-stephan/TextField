25872c23c472ef9e86cf9403b9b5031643cc9e3c
Duela Davis <dueladavis123@gmail.com>
2024-12-30 12:05:39 -0500

    sd

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,44 +1,56 @@
+@ViewBuilder
+private func HStackOrVStack<Content: View>(
+    isPad: Bool,
+    alignment: HorizontalAlignment = .center,
+    spacing: CGFloat? = nil,
+    @ViewBuilder content: () -> Content
+) -> some View {
+    if isPad {
+        HStack(alignment: .top, spacing: spacing) {
+            content()
+        }
+    } else {
+        VStack(alignment: alignment, spacing: spacing) {
+            content()
+        }
+    }
+}
+
 private func buttonSection() -> some View {
     let isPad = UIDevice.current.userInterfaceIdiom == .pad
 
-    if isPad {
-        return AnyView(
-            HStack(alignment: .top, spacing: 16) { // Replace 16 with `BankingTheme.dimens.medium`
-                SecondaryButton(
-                    content: {
-                        Text(secondaryButtonText)
-                    },
-                    buttonPaddingHorizontal: .zero,
-                    onSecondaryButtonTap
-                )
-                PrimaryButton(
-                    content: {
-                        Text(primaryButtonText)
-                    },
-                    buttonPaddingHorizontal: .zero,
-                    onPrimaryButtonTap
-                )
-            }
-        )
-    } else {
-        return AnyView(
-            VStack(alignment: .leading, spacing: 16) { // Replace 16 with `BankingTheme.dimens.medium`
-                PrimaryButton(
-                    content: {
-                        Text(primaryButtonText)
-                    },
-                    buttonPaddingHorizontal: .zero,
-                    onPrimaryButtonTap
-                )
-                SecondaryButton(
-                    content: {
-                        Text(secondaryButtonText)
-                    },
-                    buttonPaddingHorizontal: .zero,
-                    onSecondaryButtonTap
-                )
-            }
-            .padding(.top, 32) // Replace 32 with `BankingTheme.dimens.extraLarge`
-        )
+    return HStackOrVStack(isPad: isPad, alignment: .leading, spacing: BankingTheme.dimens.medium) {
+        if isPad {
+            SecondaryButton(
+                content: {
+                    Text(secondaryButtonText)
+                },
+                buttonPaddingHorizontal: .zero,
+                onSecondaryButtonTap
+            )
+            PrimaryButton(
+                content: {
+                    Text(primaryButtonText)
+                },
+                buttonPaddingHorizontal: .zero,
+                onPrimaryButtonTap
+            )
+        } else {
+            PrimaryButton(
+                content: {
+                    Text(primaryButtonText)
+                },
+                buttonPaddingHorizontal: .zero,
+                onPrimaryButtonTap
+            )
+            SecondaryButton(
+                content: {
+                    Text(secondaryButtonText)
+                },
+                buttonPaddingHorizontal: .zero,
+                onSecondaryButtonTap
+            )
+        }
     }
+    .padding(.top, BankingTheme.dimens.extraLarge)
 }
