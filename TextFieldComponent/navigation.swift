7db062881cb60709b6e3543f032cfe64ba171b88
Duela Davis <dueladavis123@gmail.com>
2024-12-30 11:43:47 -0500

    pdf

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,62 +1,44 @@
-struct BackgroundCardStyle: ViewModifier {
-    var horizontalPadding: CGFloat
-    var verticalPadding: CGFloat
-    var cornerRadius: CGFloat
-    var backgroundColor: Color
-    var outerPadding: CGFloat
+private func buttonSection() -> some View {
+    let isPad = UIDevice.current.userInterfaceIdiom == .pad
 
-    // Custom initializer with default values
-    init(
-        horizontalPadding: CGFloat = BankingTheme.dimens.small,
-        verticalPadding: CGFloat = BankingTheme.dimens.medium,
-        cornerRadius: CGFloat = 10,
-        backgroundColor: Color = BankingTheme.colors.illustrationGrey,
-        outerPadding: CGFloat = BankingTheme.dimens.medium
-    ) {
-        self.horizontalPadding = horizontalPadding
-        self.verticalPadding = verticalPadding
-        self.cornerRadius = cornerRadius
-        self.backgroundColor = backgroundColor
-        self.outerPadding = outerPadding
-    }
-
-    func body(content: Content) -> some View {
-        content
-            .padding(.horizontal, horizontalPadding)
-            .padding(.vertical, verticalPadding)
-            .background(backgroundColor)
-            .clipShape(
-                RoundedRectangle(cornerRadius: cornerRadius)
-            )
-            .padding(.all, outerPadding)
-    }
-}
-
-extension View {
-    func backgroundCardStyle(
-        horizontalPadding: CGFloat = BankingTheme.dimens.small,
-        verticalPadding: CGFloat = BankingTheme.dimens.medium,
-        cornerRadius: CGFloat = 10,
-        backgroundColor: Color = BankingTheme.colors.illustrationGrey,
-        outerPadding: CGFloat = BankingTheme.dimens.medium
-    ) -> some View {
-        modifier(
-            BackgroundCardStyle(
-                horizontalPadding: horizontalPadding,
-                verticalPadding: verticalPadding,
-                cornerRadius: cornerRadius,
-                backgroundColor: backgroundColor,
-                outerPadding: outerPadding
-            )
+    if isPad {
+        return AnyView(
+            HStack(alignment: .top, spacing: 16) { // Replace 16 with `BankingTheme.dimens.medium`
+                SecondaryButton(
+                    content: {
+                        Text(secondaryButtonText)
+                    },
+                    buttonPaddingHorizontal: .zero,
+                    onSecondaryButtonTap
+                )
+                PrimaryButton(
+                    content: {
+                        Text(primaryButtonText)
+                    },
+                    buttonPaddingHorizontal: .zero,
+                    onPrimaryButtonTap
+                )
+            }
+        )
+    } else {
+        return AnyView(
+            VStack(alignment: .leading, spacing: 16) { // Replace 16 with `BankingTheme.dimens.medium`
+                PrimaryButton(
+                    content: {
+                        Text(primaryButtonText)
+                    },
+                    buttonPaddingHorizontal: .zero,
+                    onPrimaryButtonTap
+                )
+                SecondaryButton(
+                    content: {
+                        Text(secondaryButtonText)
+                    },
+                    buttonPaddingHorizontal: .zero,
+                    onSecondaryButtonTap
+                )
+            }
+            .padding(.top, 32) // Replace 32 with `BankingTheme.dimens.extraLarge`
         )
     }
 }
- 
-Text("Custom Card Style")
-    .backgroundCardStyle(
-        horizontalPadding: 16,
-        verticalPadding: 20,
-        cornerRadius: 15,
-        backgroundColor: Color.blue,
-        outerPadding: 24
-    )
