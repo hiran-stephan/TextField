113e80667f298700e3314edecf3f41f854645d0a
Duela Davis <dueladavis123@gmail.com>
2025-01-02 13:43:15 -0500

    pdf

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -2,43 +2,10 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.SerialName
 
 @Serializable
-data class AccountPreferencesRequest(
-    @SerialName("accounts")
-    val accounts: List<AccountPreferences>,
+data class AccountPreferencesResponse(
+    @SerialName("status")
+    val status: String, // Example: SUCCESS or PARTIAL_SUCCESS
 
-    @SerialName("consent")
-    val consent: Consent
-)
-
-@Serializable
-data class AccountPreferences(
-    @SerialName("id")
-    val id: String,
-
-    @SerialName("preferences")
-    val preferences: Preferences
-)
-
-@Serializable
-data class Preferences(
-    @SerialName("estatement")
-    val estatement: Boolean,
-
-    @SerialName("nickname")
-    val nickname: String,
-
-    @SerialName("visibility")
-    val visibility: Boolean
-)
-
-@Serializable
-data class Consent(
-    @SerialName("type")
-    val type: String,
-
-    @SerialName("version")
-    val version: String,
-
-    @SerialName("acceptTimestamp")
-    val acceptTimestamp: String
+    @SerialName("problems")
+    val problems: List<BankErrorApiData>? = null
 )
