713356e4962c80150323fefdd4a752cb63503a4d
Duela Davis <dueladavis123@gmail.com>
2025-01-06 16:26:52 -0500

    sd

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,13 +1,29 @@
-SectionHeadingView(
-               heading: "Profile Settings",
-               imageName: "person.fill",
-               imageAccessibilityText: "Profile Icon",
-               trailingIcon: "chevron.right",
-               trailingIconForegroundColor: .gray,
-               trailingIconAction: {
-                   print("Chevron tapped")
-               },
-               action: {
-                   print("Heading tapped")
-               }
-           )
+public struct AccountPreferenceCardContainerView<Content: View>: View {
+    let headerText: String
+    let infoIconAccessibilityText: String?
+    let infoIconDialogBodyText: String?
+    let infoIconDialogCancelButtonText: String?
+    let showHeaderTrailingIcon: Bool?
+    let trailingIconAction: (() -> Void)?
+    @ViewBuilder let content: Content
+    
+    @State private var showTooltipAlert = false
+    
+    public init(
+        headerText: String,
+        infoIconAccessibilityText: String? = nil,
+        infoIconDialogBodyText: String? = nil,
+        infoIconDialogCancelButtonText: String? = nil,
+        showHeaderTrailingIcon: Bool? = nil,
+        trailingIconAction: (() -> Void)? = nil,
+        @ViewBuilder content: () -> Content
+    ) {
+        self.headerText = headerText
+        self.infoIconAccessibilityText = infoIconAccessibilityText
+        self.infoIconDialogBodyText = infoIconDialogBodyText
+        self.infoIconDialogCancelButtonText = infoIconDialogCancelButtonText
+        self.showHeaderTrailingIcon = showHeaderTrailingIcon
+        self.trailingIconAction = trailingIconAction
+        self.content = content()
+    }
+}
