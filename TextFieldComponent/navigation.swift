f37bbc7d97be17f0f97cf41d1a4b1888beddb16c
Duela Davis <dueladavis123@gmail.com>
2025-01-06 15:43:09 -0500

    sd

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,78 +1,9 @@
-extension AccountPreferencesDetailsScreen {
-    
-    // MARK: - Account Nickname Section
-    @ViewBuilder
-    private func accountNicknameSection() -> some View {
-        if isEditingNickname {
-            editNicknameView()
-        } else {
-            displayNicknameView()
-        }
+/// Creates an instance of `AccountPreferencesDetailsAccountPresenter` if available
+///
+/// - Returns: An optional `AccountPreferencesDetailsAccountPresenter`
+private func createAccountPresenter() -> AccountPreferencesDetailsAccountPresenter? {
+    guard let account = model?.state?.accountPreferencesAccountDetails else {
+        return nil
     }
-    
-    @ViewBuilder
-    private func editNicknameView() -> some View {
-        AccountNicknameFormView(
-            formViewModel: accountNicknameFormViewModel,
-            label: presenter.accountNicknameFieldLabelText,
-            inlineMessageText: presenter.accountNicknameInlineMessageText,
-            isValidationError: accountNicknameFormViewModel.hasValidationError.wrappedValue,
-            primaryButtonText: presenter.accountNicknameSaveButtonText,
-            secondaryButtonText: presenter.accountNicknameCancelButtonText,
-            textFieldCloseButtonTap: {
-                accountNicknameFormViewModel.clearNickname()
-            },
-            onPrimaryButtonTap: {
-                handleSaveNickname()
-            },
-            onSecondaryButtonTap: {
-                handleCancelNicknameEdit()
-            }
-        )
-    }
-    
-    @ViewBuilder
-    private func displayNicknameView() -> some View {
-        if let account = self.model?.state?.accountPreferencesAccountDetails {
-            let accountPresenter = viewModel.createAccountPresenter(account: account)
-            AccountNicknameView(
-                label: presenter.accountNicknameText,
-                hasNickname: accountPresenter.hasNickname,
-                nickname: accountPresenter.nickname,
-                buttonText: presenter.accountNicknameAddNicknameButtonText,
-                onButtonTap: {
-                    handleAddNickname()
-                }
-            )
-        }
-    }
-    
-    // MARK: - Actions
-    private func handleSaveNickname() {
-        // Validate nickname
-        let validationResult = viewModel.validateForm(
-            nickname: accountNicknameFormViewModel.nickname,
-            charLimit: 20,
-            message: presenter.accountNicknameInlineMessageText
-        )
-        
-        // Perform save if valid
-        if let id = model?.state?.accountDetails?.id {
-            accountNicknameFormViewModel.updateValidation(result: validationResult)
-            viewModel.updateAccountNickname(
-                accountId: id,
-                nickname: accountNicknameFormViewModel.nickname
-            )
-        }
-    }
-    
-    private func handleCancelNicknameEdit() {
-        isEditingNickname = false
-        accountNicknameFormViewModel.clearNickname()
-    }
-    
-    private func handleAddNickname() {
-        print("Add Nickname button tapped")
-        isEditingNickname = true
-    }
+    return viewModel.createAccountPresenter(account: account)
 }
