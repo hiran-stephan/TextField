467f8b7cc834e5b7589c8d7a76a426f45f71512c
Duela Davis <dueladavis123@gmail.com>
2025-01-06 18:42:27 -0500

    pdf

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,29 +1,3 @@
-public struct AccountPreferenceCardContainerView<Content: View>: View {
-    let headerText: String
-    let infoIconAccessibilityText: String?
-    let infoIconDialogBodyText: String?
-    let infoIconDialogCancelButtonText: String?
-    let showHeaderTrailingIcon: Bool?
-    let trailingIconAction: (() -> Void)?
-    @ViewBuilder let content: Content
+let nickname = viewModel.createAccountPresenter()?.nickname ?? ""
+        _accountNicknameFormViewModel = StateObject(wrappedValue: AccountNicknameFormViewModel(nickname: nickname))
     
-    @State private var showTooltipAlert = false
-    
-    public init(
-        headerText: String,
-        infoIconAccessibilityText: String? = nil,
-        infoIconDialogBodyText: String? = nil,
-        infoIconDialogCancelButtonText: String? = nil,
-        showHeaderTrailingIcon: Bool? = nil,
-        trailingIconAction: (() -> Void)? = nil,
-        @ViewBuilder content: () -> Content
-    ) {
-        self.headerText = headerText
-        self.infoIconAccessibilityText = infoIconAccessibilityText
-        self.infoIconDialogBodyText = infoIconDialogBodyText
-        self.infoIconDialogCancelButtonText = infoIconDialogCancelButtonText
-        self.showHeaderTrailingIcon = showHeaderTrailingIcon
-        self.trailingIconAction = trailingIconAction
-        self.content = content()
-    }
-}
