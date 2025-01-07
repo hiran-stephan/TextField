a05ffb0c7832c34489d632436303cc38802f268c
Duela Davis <dueladavis123@gmail.com>
2025-01-06 23:59:16 -0500

    sa

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,5 +1,21 @@
-.onChange(of: isEditingNickname) { newValue in
-            if newValue {
-                fetchNicknameData()
-            }
-        }
+struct AccountNicknameFormViewModelMapper {
+    static func mapToViewModel(
+        presenter: ScreenPresenter,
+        accountPresenter: AccountPresenter,
+        onPrimaryButtonTap: @escaping () -> Void,
+        onSecondaryButtonTap: @escaping () -> Void
+    ) -> AccountNicknameFormViewModel {
+        let viewData = AccountNicknameFormViewData(
+            label: presenter.accountNicknameFieldLabelText,
+            inlineMessageText: presenter.accountNicknameInlineMessageText,
+            primaryButtonText: presenter.accountNicknameSaveButtonText,
+            secondaryButtonText: presenter.accountNicknameCancelButtonText,
+            nickname: accountPresenter.nickname ?? ""
+        )
+        return AccountNicknameFormViewModel(
+            viewData: viewData,
+            onPrimaryButtonTap: onPrimaryButtonTap,
+            onSecondaryButtonTap: onSecondaryButtonTap
+        )
+    }
+}
