cf5a4e0d1df7eb52b6cb6ffe2615ae5f2463e606
Duela Davis <dueladavis123@gmail.com>
2025-01-07 15:57:17 -0500

    asd

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,45 +1,77 @@
 import SwiftUI
-import Theme
 
-public struct SectionHeadingView<TrailingContent: View>: View {
-    let heading: String
-    let imageName: String?
-    let imageAccessibilityText: String?
-    let action: (() -> Void)?
-    @ViewBuilder let trailingContent: () -> TrailingContent
-
+struct TextFieldGeneral: View {
+    @Binding var text: String
+    var enabled: Bool = true
+    var label: String
+    var labelIcon: String?
+    var leadingIcon: String?
+    var trailingIcon: String?
+    var trailingIconForegroundColor: Color?
+    var placeholder: String?
+    var isError: Bool = false
+    var errorMessage: String? // Use raw error message text
+    var infoMessage: String? // Use raw info message text
+    var trailingIconAccessibilityLabel: String?
+    var onTrailingIconClicked: (() -> Void)?
+    var onQuickTipClicked: (() -> Void)?
+    
     public init(
-        heading: String,
-        imageName: String? = nil,
-        imageAccessibilityText: String? = nil,
-        action: (() -> Void)? = nil,
-        @ViewBuilder trailingContent: @escaping () -> TrailingContent = { EmptyView() } // Default is EmptyView
+        text: Binding<String>,
+        enabled: Bool = true,
+        label: String,
+        labelIcon: String? = nil,
+        leadingIcon: String? = nil,
+        trailingIcon: String? = nil,
+        trailingIconForegroundColor: Color? = nil,
+        placeholder: String? = nil,
+        isError: Bool = false,
+        errorMessage: String? = nil,
+        infoMessage: String? = nil,
+        trailingIconAccessibilityLabel: String? = nil,
+        onTrailingIconClicked: (() -> Void)? = nil,
+        onQuickTipClicked: (() -> Void)? = nil
     ) {
-        self.heading = heading
-        self.imageName = imageName
-        self.imageAccessibilityText = imageAccessibilityText
-        self.action = action
-        self.trailingContent = trailingContent
+        self._text = text
+        self.enabled = enabled
+        self.label = label
+        self.labelIcon = labelIcon
+        self.leadingIcon = leadingIcon
+        self.trailingIcon = trailingIcon
+        self.trailingIconForegroundColor = trailingIconForegroundColor
+        self.placeholder = placeholder
+        self.isError = isError
+        self.errorMessage = errorMessage
+        self.infoMessage = infoMessage
+        self.trailingIconAccessibilityLabel = trailingIconAccessibilityLabel
+        self.onTrailingIconClicked = onTrailingIconClicked
+        self.onQuickTipClicked = onQuickTipClicked
     }
-
+    
     public var body: some View {
-        HStack(alignment: .center, spacing: BankingTheme.spacing.noPadding) {
-            Text(heading.uppercased())
-                .typography(BankingTheme.typography.allCapsHeading)
-                .foregroundColor(BankingTheme.colors.textPrimary)
+        VStack(alignment: .leading, spacing: 8) {
+            // Text Field and Other Content
+            TextField(placeholder ?? "", text: $text)
+                .disabled(!enabled)
+                .padding()
+                .background(RoundedRectangle(cornerRadius: 8).stroke(isError ? Color.red : Color.gray))
 
-            Spacer()
-
-            if let imageName = imageName {
-                Button(action: {
-                    action?()
-                }) {
-                    Image(systemName: imageName)
-                        .accessibility(label: Text(imageAccessibilityText ?? ""))
-                }
+            // Error Message
+            if let errorMessage = errorMessage, isError {
+                InlineAlert(
+                    statusMessage: errorMessage,
+                    statusType: "error"
+                )
             }
 
-            trailingContent()
+            // Info Message
+            if let infoMessage = infoMessage, !isError {
+                InlineAlert(
+                    statusMessage: infoMessage,
+                    statusType: "information"
+                )
+            }
         }
+        .padding()
     }
 }
