d9dddca7c1d4134ee5db1e3476cec60064111fc6
Duela Davis <dueladavis123@gmail.com>
2025-01-09 18:29:32 -0500

    pdf

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,18 +1,41 @@
-@ViewBuilder
-private func editNicknameView() -> some View {
-    if let accountNicknameFormViewModel = self.accountNicknameFormViewModel,
-       let state = self.model.state {
-        AccountNicknameFormView(
-            formViewModel: accountNicknameFormViewModel,
-            isEditingNickname: Binding(
-                get: { state.isEditingNickname },
-                set: { newValue in
-                    model.state?.isEditingNickname = newValue
-                }
-            )
-        ) {
-            // Save button action
-            handleSaveNickname()
-        }
+struct AccountNicknameFormViewModelMapper {
+    static func mapToViewModelData(
+        presenter: AccountPreferencesDetailsScreenPresenter,
+        accountPresenter: AccountPreferencesDetailsAccountPresenter
+    ) -> AccountNicknameFormViewData {
+        return AccountNicknameFormViewData(
+            label: presenter.accountNicknameFieldLabelText,
+            inlineMessageText: presenter.accountNicknameInlineMessageText,
+            primaryButtonText: presenter.accountNicknameSaveButtonText,
+            secondaryButtonText: presenter.accountNicknameCancelButtonText,
+            nickname: accountPresenter.nickname ?? ""
+        )
     }
 }
+
+
+struct ViewModelFactory {
+    static func createAccountNicknameFormViewModel(
+        presenter: AccountPreferencesDetailsScreenPresenter,
+        accountPresenter: AccountPreferencesDetailsAccountPresenter
+    ) -> AccountNicknameFormViewModel {
+        let viewData = AccountNicknameFormViewModelMapper.mapToViewModelData(
+            presenter: presenter,
+            accountPresenter: accountPresenter
+        )
+        return AccountNicknameFormViewModel(viewData: viewData)
+    }
+}
+
+
+@State private lazy var accountNicknameFormViewModel: AccountNicknameFormViewModel? = {
+    if let accountPresenter = createAccountPresenter() {
+        return ViewModelFactory.createAccountNicknameFormViewModel(
+            presenter: viewModel.createScreenPresenter(),
+            accountPresenter: accountPresenter
+        )
+    }
+    return nil
+}()
+
+
