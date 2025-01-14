e5a44a05b7200a385ec858a2235d611d7af9a58f
Duela Davis <dueladavis123@gmail.com>
2025-01-14 15:03:38 -0500

    asd

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -15,3 +15,22 @@ fun createProblemsListPresenter(
 ): List<ProblemsPresenter> {
     return ProblemsListPresenter(error?.toProblemsData() ?: emptyList(), messageCatalogue).getPresenters()
 }
+
+
+@ViewBuilder
+private func errorInlineView() -> some View {
+    // Create the error presenters
+    let errorPresenters = viewModel.createProblemsListPresenter()
+
+    VStack(spacing: BankingTheme.spacing.noPadding) {
+        ForEach(errorPresenters.indices, id: \.self) { index in
+            let presenter = errorPresenters[index]
+            errorInlineListView(
+                alertType: AlertType.failure.rawValue,
+                alertMessage: presenter.formatGlobalAlertMessage(),
+                alertCode: presenter.formatErrorForGlobalAlertCode(),
+                applyPadding: true
+            )
+        }
+    }
+}
