ce318f88554a978d0fc053b3297e06eef7ab89b6
Duela Davis <dueladavis123@gmail.com>
2025-01-21 11:23:56 -0500

    pdf

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,56 +1,58 @@
-import SwiftUI
-
-public protocol CardStyle {
-    func body<Content: View>(content: Content) -> AnyView
+public protocol EmptyStateStyle {
+    associatedtype Body: View
+    func body(content: AnyView) -> Body
 }
 
-
-public struct DefaultEmptyStateStyle: CardStyle {
-    var backgroundColor: Color = BankingTheme.colors.illustrationGrey
-    var cornerRadius: CGFloat = 12
-
-    public func body<Content: View>(content: Content) -> AnyView {
-        AnyView(
-            content
-                .padding(.horizontal, 28)
-                .padding(.vertical, 60)
-                .background(backgroundColor)
-                .cornerRadius(cornerRadius)
-        )
-    }
-}
-
-public struct PlainEmptyStateStyle: CardStyle {
-    public func body<Content: View>(content: Content) -> AnyView {
-        AnyView(
-            content
-                .padding()
-        )
-    }
-}
-
-
-public struct EmptyState<Content: View, Style: CardStyle>: View {
-    private let content: () -> Content
+public struct EmptyState<Style: EmptyStateStyle, ImageView: View, ContentView: View, ActionView: View>: View {
     private let style: Style
+    private let imageView: (() -> ImageView)?
+    private let contentView: (() -> ContentView)?
+    private let actionView: (() -> ActionView)?
 
     public init(
         style: Style,
-        @ViewBuilder content: @escaping () -> Content
+        @ViewBuilder imageView: (() -> ImageView)? = nil,
+        @ViewBuilder contentView: (() -> ContentView)? = nil,
+        @ViewBuilder actionView: (() -> ActionView)? = nil
     ) {
         self.style = style
-        self.content = content
+        self.imageView = imageView
+        self.contentView = contentView
+        self.actionView = actionView
     }
 
     public var body: some View {
-        style.body(content: content())
+        let content = VStack(spacing: 16) {
+            if let imageView = imageView {
+                imageView()
+            }
+            if let contentView = contentView {
+                contentView()
+            }
+            if let actionView = actionView {
+                actionView()
+            }
+        }.eraseToAnyView()
+
+        return style.body(content: content)
     }
 }
 
 
-EmptyState(style: DefaultEmptyStateStyle()) {
-    VStack {
-        Text("Default Empty State")
-        Image(systemName: "exclamationmark.circle")
+
+public struct DefaultEmptyStateStyle: EmptyStateStyle {
+    public func body(content: AnyView) -> some View {
+        content
+            .padding(.horizontal, 28)
+            .padding(.vertical, 60)
+            .background(BankingTheme.colors.illustrationGrey)
+            .cornerRadius(12)
+    }
+}
+
+public struct PlainEmptyStateStyle: EmptyStateStyle {
+    public func body(content: AnyView) -> some View {
+        content
+            .padding()
     }
 }
