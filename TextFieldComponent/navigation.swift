63c8f7e6793b0e2486008fc3d0d250ab9e14490f
Duela Davis <dueladavis123@gmail.com>
2025-01-21 11:15:44 -0500

    asd

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,11 +1,56 @@
-struct EmptyState: View {
-    var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                Text("Your Content Here")
-            }
-            .frame(width: geometry.size.width, height: geometry.size.height)
-            .background(Color.yellow)
-        }
+import SwiftUI
+
+public protocol CardStyle {
+    func body<Content: View>(content: Content) -> AnyView
+}
+
+
+public struct DefaultEmptyStateStyle: CardStyle {
+    var backgroundColor: Color = BankingTheme.colors.illustrationGrey
+    var cornerRadius: CGFloat = 12
+
+    public func body<Content: View>(content: Content) -> AnyView {
+        AnyView(
+            content
+                .padding(.horizontal, 28)
+                .padding(.vertical, 60)
+                .background(backgroundColor)
+                .cornerRadius(cornerRadius)
+        )
+    }
+}
+
+public struct PlainEmptyStateStyle: CardStyle {
+    public func body<Content: View>(content: Content) -> AnyView {
+        AnyView(
+            content
+                .padding()
+        )
+    }
+}
+
+
+public struct EmptyState<Content: View, Style: CardStyle>: View {
+    private let content: () -> Content
+    private let style: Style
+
+    public init(
+        style: Style,
+        @ViewBuilder content: @escaping () -> Content
+    ) {
+        self.style = style
+        self.content = content
+    }
+
+    public var body: some View {
+        style.body(content: content())
+    }
+}
+
+
+EmptyState(style: DefaultEmptyStateStyle()) {
+    VStack {
+        Text("Default Empty State")
+        Image(systemName: "exclamationmark.circle")
     }
 }
