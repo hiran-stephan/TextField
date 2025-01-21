84f61a65befd7dbe771230ce1c008a25ac74b25d
Duela Davis <dueladavis123@gmail.com>
2025-01-20 19:08:48 -0500

    pdf

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,60 +1,11 @@
-public struct StyledImageView<Style: ImageViewStyle>: View {
-    private let image: Image
-    private let style: Style
-
-    public init(
-        image: Image,
-        style: Style = CircularImageViewStyle() as! Style // Default to CircularImageViewStyle
-    ) {
-        self.image = image
-        self.style = style
-    }
-
-    public var body: some View {
-        style.body(content: image)
-    }
-}
-
-
-public protocol ImageViewStyle {
-    associatedtype Body: View
-    func body(content: Image) -> Body
-}
-
-public struct CircularImageViewStyle: ImageViewStyle {
-    let backgroundColor: Color
-    let size: CGFloat
-    let padding: CGFloat
-
-    public init(
-        backgroundColor: Color = Color.gray.opacity(0.2),
-        size: CGFloat = 100,
-        padding: CGFloat = 16
-    ) {
-        self.backgroundColor = backgroundColor
-        self.size = size
-        self.padding = padding
-    }
-
-    public func body(content: Image) -> some View {
-        ZStack {
-            Circle()
-                .fill(backgroundColor)
-                .frame(width: size + padding, height: size + padding)
-            content
-                .resizable()
-                .scaledToFit()
-                .frame(width: size, height: size)
+struct EmptyState: View {
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                Text("Your Content Here")
+            }
+            .frame(width: geometry.size.width, height: geometry.size.height)
+            .background(Color.yellow)
         }
-    }
-}
-
-public struct BackgroundlessImageViewStyle: ImageViewStyle {
-    public init() {}
-
-    public func body(content: Image) -> some View {
-        content
-            .resizable()
-            .scaledToFit()
     }
 }
