f5d7ba5ac74f82140a931783cdd24e9c71389982
Duela Davis <dueladavis123@gmail.com>
2025-02-04 14:52:11 -0500

    sd

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,22 +1,51 @@
-private var tooltipAlertModifier: some ViewModifier {
-    Group {
+struct TooltipAlertModifier: ViewModifier {
+    let showTooltipAlert: Binding<Bool>?
+    let infoIconDialogBodyText: String?
+    let infoIconDialogCancelButtonText: String?
+
+    func body(content: Content) -> some View {
         if let showTooltipAlert = showTooltipAlert {
-            PresentAlertModifier(
-                isPresented: showTooltipAlert,
-                title: "",
-                message: infoIconDialogBodyText ?? "",
-                actions: [
-                    AlertAction(
-                        title: infoIconDialogCancelButtonText ?? "",
-                        style: .default,
-                        handler: { showTooltipAlert.wrappedValue = false }
-                    )
-                ]
-            )
+            content
+                .presentAlert(
+                    isPresented: showTooltipAlert,
+                    title: "",
+                    message: infoIconDialogBodyText ?? "",
+                    actions: [
+                        AlertAction(
+                            title: infoIconDialogCancelButtonText ?? "",
+                            style: .default,
+                            handler: { showTooltipAlert.wrappedValue = false }
+                        )
+                    ]
+                )
         } else {
-            EmptyModifier()
+            content  // If there's no alert, return content as-is
         }
     }
 }
 
-.modifier(tooltipAlertModifier)
+public var body: some View {
+    VStack(alignment: .leading, spacing: BankingTheme.spacing.noPadding) {
+        SectionHeadingView(
+            headerText,
+            imageName: (infoIconDialogBodyText != nil) ?
+                BankingTheme.icons.functional.infoGrey.rawValue : nil,
+            imageAccessibilityText: infoIconAccessibilityText,
+            trailingContent: {
+                trailingContent()
+            }
+        ) {
+            if let showTooltipAlert = showTooltipAlert {
+                showTooltipAlert.wrappedValue = true
+            }
+        }
+        
+        content
+    }
+    .modifier(TooltipAlertModifier(
+        showTooltipAlert: showTooltipAlert,
+        infoIconDialogBodyText: infoIconDialogBodyText,
+        infoIconDialogCancelButtonText: infoIconDialogCancelButtonText
+    ))
+}
+
