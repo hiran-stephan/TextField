c0ae4a763ab7d88256d69d8552b3b78f8c7f3539
Duela Davis <dueladavis123@gmail.com>
2025-02-04 14:47:52 -0500

    asd

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,116 +1,22 @@
-@OptIn(ExperimentalCoroutinesApi::class)
-class AccountPreferencesRepositoryTest {
-
-    private val mockRemoteResourceApiService = mock<RemoteResourceApiService>()
-    private val mockAccountsApiService = mock<AccountsApiService>()
-    private val mockAccountCatalogue = mock<AccountCatalogue>()
-    private val mockConnectivityChecker = mock<NetworkConnectivityChecker>()
-    private val dispatcher = UnconfinedTestDispatcher()
-
-    private lateinit var repository: AccountPreferencesRepository
-
-    @BeforeTest
-    fun setup() {
-        Dispatchers.setMain(dispatcher)
-
-        repository = AccountPreferencesRepositoryImpl(
-            mockRemoteResourceApiService,
-            mockAccountsApiService,
-            mockAccountCatalogue
-        )
-    }
-
-    @AfterTest
-    fun tearDown() {
-        Dispatchers.resetMain()
+private var tooltipAlertModifier: some ViewModifier {
+    Group {
+        if let showTooltipAlert = showTooltipAlert {
+            PresentAlertModifier(
+                isPresented: showTooltipAlert,
+                title: "",
+                message: infoIconDialogBodyText ?? "",
+                actions: [
+                    AlertAction(
+                        title: infoIconDialogCancelButtonText ?? "",
+                        style: .default,
+                        handler: { showTooltipAlert.wrappedValue = false }
+                    )
+                ]
+            )
+        } else {
+            EmptyModifier()
+        }
     }
 }
 
-@Test
-fun `test loadAccounts successful`() = runTest {
-    val accountSummaryApiData = getAccountsSummaryApiData()
-    val accountGroups = mapAccountGroups(
-        accountCatalogue = mockAccountCatalogue,
-        accountSorter = mockAccountSorter,
-        response = accountSummaryApiData
-    ).filter { it.accounts.isNotEmpty() }
-
-    val accountPreferencesAccountsData = AccountPreferencesAccountsData(
-        accountGroups = accountGroups,
-        problems = accountSummaryApiData.problems?.map { it.toProblemData() }
-    )
-    every { mockAccountSorter.sort(any<List<AccountPreferencesAccount>>()) } answers { firstArg() }
-
-    every { mockConnectivityChecker.isConnected() } returns true
-    every { mockAccountsApiService.fetchAccounts(any()) } returns accountSummaryApiData
-    every { mockAccountCatalogue.formatAccountDisplayName(any(), any(), any()) } returns "Mocked Account Name"
-
-    val result = repository.loadAccounts().last()
-
-    (result as? NetworkResultState.Success)?.onSuccess { response ->
-        assertEquals(accountPreferencesAccountsData, response)
-    }
-}
-
-
-
-@Test
-fun `test loadAccounts failure`() = runTest {
-    everySuspend { mockAccountsApiService.fetchAccounts(any()) } throws Exception("API Error")
-
-    val result = repository.loadAccounts().last()
-
-    (result as? NetworkResultState.Failure)?.onException { response ->
-        assertEquals("API Error", response.message)
-    }
-}
-
-
-
-@Test
-fun `test fetchAccountById successful`() = runTest {
-    val accountId = "123"
-    val mockAccountData = AccountPreferencesAccountData(account = mockAccountDetails(), problems = emptyList())
-
-    everySuspend { mockAccountsApiService.fetchAccountById(any(), eq(accountId)) } returns mockAccountData
-
-    val result = repository.fetchAccountById(accountId).last()
-
-    (result as? NetworkResultState.Success)?.onSuccess { response ->
-        assertEquals(mockAccountData, response)
-    }
-}
-
-
-@Test
-fun `test updateAccountNickname successful`() = runTest {
-    val accountId = "123"
-    val nickname = "New Nickname"
-    val mockAccountData = AccountPreferencesAccountData(account = mockAccountDetails(), problems = emptyList())
-
-    everySuspend { mockAccountsApiService.updateAccountNickname(any(), eq(accountId), eq(nickname)) } returns Unit
-    everySuspend { mockAccountsApiService.fetchAccountById(any(), eq(accountId)) } returns mockAccountData
-
-    val result = repository.updateAccountNickname(accountId, nickname).last()
-
-    (result as? NetworkResultState.Success)?.onSuccess { response ->
-        assertEquals(mockAccountData, response)
-    }
-}
-
-
-@Test
-fun `test updateAccountVisibility successful`() = runTest {
-    val accountId = "123"
-    val visibility = true
-    val mockAccountData = AccountPreferencesAccountData(account = mockAccountDetails(), problems = emptyList())
-
-    everySuspend { mockAccountsApiService.updateAccountVisibility(any(), eq(accountId), eq(visibility)) } returns Unit
-    everySuspend { mockAccountsApiService.fetchAccountById(any(), eq(accountId)) } returns mockAccountData
-
-    val result = repository.updateAccountVisibility(accountId, visibility).last()
-
-    (result as? NetworkResultState.Success)?.onSuccess { response ->
-        assertEquals(mockAccountData, response)
-    }
-}
+.modifier(tooltipAlertModifier)
