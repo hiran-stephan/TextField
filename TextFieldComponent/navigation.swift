9972fe95a5fef42d615210ae454b93c67c4fed67
Duela Davis <dueladavis123@gmail.com>
2025-02-13 08:34:53 -0500

    pdf

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,95 +1,18 @@
-sealed class ConsentsNavigationItems(val path: String) {
+fun Map<String, List<String>>.createRedirectDeepLink(key: String): String? {
+    if (isNotEmpty()) {
+        val deepLink = StringBuilder()
+        val redirectToValue = this[key]?.firstOrNull()
 
-    data class Main(
-        val redirect: String? = null // Holds the redirect destination
-    ) : ConsentsNavigationItems(path = "consents") {
-
-        // Function to generate deep link
-        fun toDeepLink(): String {
-            return if (!redirect.isNullOrEmpty()) {
-                "cibcus://consents?redirect=$redirect"
-            } else {
-                "cibcus://consents"
-            }
+        return redirectToValue?.let { redirectTo ->
+            val queryString = this.filterNot { it.key == key }.toQueryString()
+            deepLink
+                .append("cibcus://$redirectTo")
+                .append(queryString)
+                .toString()
         }
     }
+    return null
 }
 
-fun parse(url: String): ConsentsNavigationItems.Main {
-    val params = url.parseQueryString()
-    val redirect = params["redirect"]?.firstOrNull()
-
-    return ConsentsNavigationItems.Main(redirect = redirect)
-}
-
-
-fun navigateToConsents(rememberMeClicked: Boolean) {
-    val navigationItem = if (rememberMeClicked) {
-        ConsentsNavigationItems.Main(redirect = "settings/customize")
-    } else {
-        ConsentsNavigationItems.Main()
-    }
-
-    router.navigateTo(navigationItem)
-}
-
-class ConsentsPageRouter(...) {
-
-    fun navigateTo(item: ConsentsNavigationItems.Main) {
-        val deepLink = item.toDeepLink()
-        parent.navigateTo(DeeplinkNavigationItem(Url(deepLink)))
-    }
-}
-sealed class ConsentsNavigationItems(
-    override val path: String
-) : NavigationItem() {
-
-    companion object {
-        const val DOMAIN = "consents"
-
-        fun parse(url: String): Main {
-            val params = url.parseQueryString()
-            val redirect = params["redirect"]?.firstOrNull()
-            return Main(redirect = redirect)
-        }
-
-        fun cast(item: NavigationItem): Main {
-            return item as? Main ?: parse(item.deeplink)
-        }
-    }
-
-    // 1) data class with domain overridden, path = "" so route is "/consents"
-    data class Main(
-        val redirect: String? = null
-    ) : ConsentsNavigationItems(path = "") {
-
-        // 2) Override domain so that route = "/consents"
-        override val domain: String get() = DOMAIN
-
-        // 3) Optionally give yourself a deep-link builder
-        fun toDeepLink(): String =
-            if (!redirect.isNullOrEmpty()) {
-                "cibcus://consents?redirect=$redirect"
-            } else {
-                "cibcus://consents"
-            }
-    }
-
-    // etc...
-}
-
-data class Main(
-    val redirectTo: String? = null
-) : ConsentsNavigationItems(path = "consents") {
-
-    override val route: String
-        get() = "/$path"
-
-    override val deeplink: String
-        get() = "/$path" + redirectTo?.let { "?redirect=$it" }.orEmpty()
-}
-
-
-single<FeatureRouter>(named("localhost-router")) {
-    get<ConsentsFeatureRouter>()  // or some other router
-}
+val deepLink1 = myMap.createRedirectDeepLink(KEY_PARAM_REDIRECT_TO)
+val deepLink2 = myMap.createRedirectDeepLink(KEY_PARAM_REDIRECT)
