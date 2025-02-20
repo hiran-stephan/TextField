a5b24a88b433d0a298d662f7111fd361523ab4d2
Duela Davis <dueladavis123@gmail.com>
2025-02-20 10:39:36 -0500

    sdf

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,62 +1,57 @@
-struct ConsentCaptureListCell: View {
-    struct Data: Hashable {
-        let title: String
-        let badgeText: String
-    }
+import SwiftUI
 
-    let data: Data
+struct DocumentData {
+    let documents: [Document]
+}
 
-    @Environment(\.isLastRow) private var isLastRow: Bool
+struct Document: Identifiable {
+    let id = UUID()
+    let title: String
+    let type: String
+    let path: String
+    let badgeText: String
+    let errorMessage: String?
+}
+
+struct ConsentCaptureListCardView: View {
+    let data: DocumentData
+    let onChangeDocumentReviewStatus: (String, Bool) -> Void
 
     var body: some View {
-        VStack(alignment: .leading, spacing: BankingTheme.spacing.noPadding) {
-            HStack(alignment: .center, spacing: BankingTheme.dimens.smallMedium) {
-                HStack(alignment: .center, spacing: BankingTheme.dimens.small) {
-                    ComponentImage(BankingTheme.icons.functional.pdf.rawValue)
-                }
-
-                Text(data.title)
-                    .underline()
-                    .typography(BankingTheme.typography.body)
-                    .foregroundColor(BankingTheme.colors.textPrimary)
-                    .frame(maxWidth: .infinity, alignment: .topLeading)
+        LazyVStack(alignment: .leading, spacing: BankingTheme.spacing.noPadding) {
+            if let firstDocument = data.documents.first {
+                documentCell(for: firstDocument)
             }
-            .padding(.vertical, BankingTheme.dimens.small)
-            .frame(maxWidth: .infinity, alignment: .leading)
-
-            Spacer()
-
-            BadgeIndicator(BadgeIndicatorData(type: .passive, text: data.badgeText))
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(BankingTheme.dimens.medium)
-
-            // Automatically hide the divider for the last row
-            if !isLastRow {
-                Divider()
-                    .frame(height: 1)
-                    .background(BankingTheme.colors.illustrationGrey)
-                    .padding(.horizontal, BankingTheme.dimens.medium)
+            
+            ForEach(data.documents.dropFirst()) { document in
+                createDivider()
+                documentCell(for: document)
             }
         }
+        .cornerRadius(BankingTheme.dimensions.smallMedium)
+        .modifier(TileViewModifier())
     }
-}
-
-
-
-
-private struct IsLastRowKey: EnvironmentKey {
-    static let defaultValue: Bool = false
-}
-
-extension EnvironmentValues {
-    var isLastRow: Bool {
-        get { self[IsLastRowKey.self] }
-        set { self[IsLastRowKey.self] = newValue }
+    
+    @ViewBuilder
+    private func documentCell(for document: Document) -> some View {
+        ConsentCaptureListCell(
+            data: ConsentCaptureListCell.Data(
+                title: document.title,
+                badgeText: document.badgeText,
+                errorMessage: document.errorMessage
+            ),
+            onReviewDocument: {
+                onChangeDocumentReviewStatus(document.type, true)
+            }
+        )
     }
-}
 
-
-List(dataArray.indices, id: \.self) { index in
-    ConsentCaptureListCell(data: dataArray[index])
-        .environment(\.isLastRow, index == dataArray.count - 1)
+    @ViewBuilder
+    private func createDivider() -> some View {
+        Divider()
+            .frame(height: BankingTheme.spacing.stroke)
+            .background(BankingTheme.colors.borderDefault)
+            .accessibilityHidden(true)
+            .padding(.horizontal, BankingTheme.dimensions.medium)
+    }
 }
