11835f23e734cab8e4253716304d45628a33b339
Duela Davis <dueladavis123@gmail.com>
2025-02-20 14:47:35 -0500

    asd

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,10 +1,38 @@
-extension ConsentStyle: Equatable {
-    static func == (lhs: ConsentStyle, rhs: ConsentStyle) -> Bool {
-        return lhs.uncheckedIcon === rhs.uncheckedIcon &&
-               lhs.checkedIcon === rhs.checkedIcon &&
-               lhs.typography == rhs.typography &&
-               lhs.backgroundColor == rhs.backgroundColor &&
-               lhs.borderColor == rhs.borderColor &&
-               lhs.alignment == rhs.alignment
+struct ConsentCaptureSectionView: View {
+    let data: ConsentSectionData
+    let onChangeConsent: (Bool) -> Void
+    let onChangeDocumentReviewStatus: (String, Bool) -> Void
+
+    private var consentStyle: ConsentStyle {
+        if let errorMessage = data.consentData.errorMessage, !errorMessage.isEmpty {
+            return ConsentStyle.error
+        } else if data.consentData.isChecked {
+            return ConsentStyle.checkbox
+        } else {
+            return ConsentStyle.readOnly
+        }
+    }
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: BankingTheme.dimens.smallMedium) {
+            // Section Title
+            Text(data.title)
+                .typography(BankingTheme.typography.body)
+                .foregroundColor(.black)
+                .frame(maxWidth: .infinity, alignment: .topLeading)
+
+            // List of PDF Links
+            ConsentCaptureListCardView(
+                data: data.captureListCardData,
+                onChangeDocumentReviewStatus: onChangeDocumentReviewStatus
+            )
+
+            // Consent View
+            ConsentView(
+                data: data.consentData,
+                style: consentStyle,
+                onClickCheckbox: onChangeConsent
+            )
+        }
     }
 }
