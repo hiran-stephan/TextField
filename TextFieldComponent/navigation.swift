7a704c68921b100f09949c6087e6f1c229640077
Duela Davis <dueladavis123@gmail.com>
2025-02-26 18:35:43 -0500

    pdf

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,24 +1,4 @@
-extension ConsentSectionPresenter {
-    func toConsentSectionData() -> [ConsentSectionData] {
-        return sections.map { section in
-            ConsentSectionData(
-                title: getSectionTitle(consentType: section.consentData.consentType),
-                captureListCardData: DocumentData(documents: section.consentDocuments.map { consentDocument in
-                    ConsentDocument(
-                        id: UUID(),
-                        title: consentDocument.consentName,
-                        type: consentDocument.consentType,
-                        path: consentDocument.consentPath,
-                        badgeText: "", // Placeholder for badge text if needed
-                        errorMessage: consentDocument.error
-                    )
-                }),
-                consentData: ConsentData(
-                    text: getConsentText(consentType: section.consentData.consentType),
-                    isChecked: isConsentRequired(consentType: section.consentData.consentType),
-                    errorMessage: section.consentData.consentError
-                )
-            )
-        }
-    }
-}
+val consentDocumentBadgeText: String = if (consentData?.isReviewed == true)
+    reviewedStatusPillText 
+else
+    pendingReviewStatusPillText
