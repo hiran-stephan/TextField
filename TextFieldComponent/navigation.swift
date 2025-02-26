da950c5674d8ee288d53930db2fbcaa6adbc9243
Duela Davis <dueladavis123@gmail.com>
2025-02-26 18:40:25 -0500

    sd

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,4 +1,21 @@
-val consentDocumentBadgeText: String = if (consentData?.isReviewed == true)
-    reviewedStatusPillText 
-else
-    pendingReviewStatusPillText
+val consentDocuments = consentData2?.map { consent ->
+    ConsentDocumentMapped(
+        consentDocumentTitle = consent.consentName ?: "",
+        consentDocumentType = consent.consentType ?: "",
+        consentDocumentVersion = consent.consentVersion ?: "",
+        consentDocumentPath = consent.consentPath ?: "",
+        isConsentDocumentReviewed = consent.isReviewed ?: false,
+        consentDocumentError = consent.error ?: "",
+        consentDocumentBadgeText = if (consent.isReviewed == true) reviewedStatusPillText else pendingReviewStatusPillText
+    )
+} ?: emptyList()
+
+data class ConsentDocumentMapped(
+    val consentDocumentTitle: String,
+    val consentDocumentType: String,
+    val consentDocumentVersion: String,
+    val consentDocumentPath: String,
+    val isConsentDocumentReviewed: Boolean,
+    val consentDocumentError: String,
+    val consentDocumentBadgeText: String
+)
