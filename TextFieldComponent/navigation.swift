14a0540b9740971a5e9e0d49927af181cf0eef8b
Duela Davis <dueladavis123@gmail.com>
2025-02-26 11:51:51 -0500

    pdf

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,16 +1,18 @@
-fun ConsentsViewModel.onConsentsErrorStateChanged() {
+fun ConsentsViewModel.onCheckboxCheckedChange(consentType: String) {
     _consentUiState.value = _consentUiState.value.copy(
         data = _consentUiState.value.data?.let { consentsData ->
             consentsData.copy(
                 sections = consentsData.sections.map { section ->
-                    section.copy(
-                        consentDocuments = section.consentDocuments.map { document ->
-                            document.copy(
-                                error = if (document.isReviewed) null
-                                else "Error message will come from presenter {#000}"
+                    if (section.consentData.consentText == consentType) {
+                        // Toggle isConsentProvided only for the matching section
+                        section.copy(
+                            consentData = section.consentData.copy(
+                                isConsentProvided = !section.consentData.isConsentProvided
                             )
-                        }
-                    )
+                        )
+                    } else {
+                        section
+                    }
                 }
             )
         }
