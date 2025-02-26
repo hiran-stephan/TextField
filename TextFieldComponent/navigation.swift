a20c8a0a351c5c42ca1e15587ece51c01e577777
Duela Davis <dueladavis123@gmail.com>
2025-02-26 11:27:53 -0500

    sad

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,61 +1,24 @@
 fun ConsentsViewModel.onConsentReviewed(consentData: ConsentDocumentsData) {
     _consentUiState.update { previousState ->
         previousState.copy(
-            data = previousState.data?.updateReviewStatus(consentData)
+            data = previousState.data?.updateDocumentReviewStatus(consentData)
         )
     }
 }
 
-fun ConsentsViewModel.onConsentsErrorStateChanged() {
-    _consentUiState.value = _consentUiState.value.copy(
-        data = _consentUiState.value.data?.let { consentsData ->
-            consentsData.copy(
-                sections = consentsData.sections.map { section ->
-                    section.copy(
-                        consentData = section.consentData.copy(
-                            consentError = if (section.consentData.isConsentProvided) null
-                            else "Error message will come from presenter {#000}"
-                        )
-                    )
-                }
-            )
-        }
-    )
-}
-
-fun ConsentsViewModel.onCheckboxCheckedChange() {
-    _consentUiState.value = _consentUiState.value.copy(
-        isCheckboxChecked = !_consentUiState.value.isCheckboxChecked
-    )
-}
-
-fun ConsentsViewModel.onConsentMethodErrorChanged() {
-    val errorText = if (_consentUiState.value.isCheckboxChecked) null
-    else "Error message from presenter {#000}"
-
-    _consentUiState.value = _consentUiState.value.copy(
-        consentMethodError = errorText
-    )
-}
-
-
-fun ConsentsData.updateReviewStatus(consentData: ConsentData): ConsentsData = copy(
+fun ConsentsData.updateDocumentReviewStatus(consentData: ConsentDocumentsData): ConsentsData = copy(
     sections = sections.map { section ->
         section.copy(
-            consentData = section.consentData.updateReviewStatusIfMatch(consentData)
+            consentDocuments = section.consentDocuments.map { document ->
+                document.updateReviewStatusIfMatch(consentData)
+            }
         )
     }
 )
 
-fun ConsentData.updateReviewStatusIfMatch(consentData: ConsentData): ConsentData =
-    if (this.consentText == consentData.consentText) {
-        copy(isConsentProvided = !this.isConsentProvided)
+fun ConsentDocumentsData.updateReviewStatusIfMatch(consentData: ConsentDocumentsData): ConsentDocumentsData =
+    if (this.consentName == consentData.consentName && this.consentVersion == consentData.consentVersion) {
+        copy(isReviewed = !this.isReviewed)
     } else {
         this
     }
-
-fun ConsentsData.determineConsentError(): String? {
-    return sections.firstOrNull { !it.consentData.isConsentProvided }?.let {
-        "You must review consent for ${it.title}."
-    }
-}
