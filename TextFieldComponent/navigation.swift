34ec75e384f3a55da01e2316985b7d75be5b8228
Duela Davis <dueladavis123@gmail.com>
2025-02-26 20:03:43 -0500

    sd

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,26 +1,25 @@
-fun getSectionTitle(consentType: String): String {
-      return when (consentType) {
-          "13" -> displayContent(ContentConstants.STEP_1_TITLE_TEXT)
-          "14" -> displayContent(ContentConstants.STEP_2_TITLE_TEXT)
-          else -> "Unknown Consent Type"
-      }
-  }
-
-  // Fetch Consent Text based on ConsentType
-  fun getConsentText(consentType: String): String {
-      return when (consentType) {
-          "13" -> displayContent(ContentConstants.CONSENTS_CHECKBOX_TEXT_FOR_BDSA_AGREEMENT_WHEN_EDCA_AGREEMENT_AVAILABLE)
-          "14" -> displayContent(ContentConstants.CONSENTS_CHECKBOX_TEXT_FOR_EDCA_AGREEMENT_ONLY)
-          else -> "Consent text not available"
-      }
-  }
-
-  // Check if Consent is required based on ConsentType
-  fun isConsentRequired(consentType: String): Boolean {
-      return when (consentType) {
-          "13" -> true
-          "14" -> false
-          else -> false
-      }
-  }
-val consentType: String = consentData.firstOrNull()?.consentType ?: ""
+extension ConsentSectionPresenter {
+    func toSectionData() -> SectionData {
+        return SectionData(
+            id: UUID(),
+            title: title, // Directly assigning the title from ConsentSectionPresenter
+            documents: consentData.flatMap { consent in
+                consent.documents.map { document in
+                    ConsentDocument(
+                        id: UUID(),
+                        title: document.consentName,
+                        type: document.consentType,
+                        path: document.consentPath,
+                        badgeText: document.isReviewed ? reviewedStatusPillText : pendingReviewStatusPillText,
+                        errorMessage: document.error
+                    )
+                }
+            },
+            consent: ConsentData(
+                text: getConsentText(consentType: consentData.firstOrNull()?.consentType ?? "Unknown"),
+                isChecked: isCheckboxChecked,
+                errorMessage: isConsentError ? "Check the box to confirm you've read and accepted all the presented terms." : nil
+            )
+        )
+    }
+}
