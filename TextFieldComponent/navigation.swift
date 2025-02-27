16c8b9c4030df7eabcb67abbc094a12281f85fe7
Duela Davis <dueladavis123@gmail.com>
2025-02-27 12:49:28 -0500

    pdf

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,70 +1,22 @@
-package com.cibc.consents.ui.screens.consents
+/** --- Extension Functions --- **/
 
-import kotlinx.coroutines.flow.update
-
-class ConsentsViewModel {
-
-    /** --- UI State Management --- **/
-    
-    fun onSubmitButtonClick() {
-        val redirect = navigationItem.redirect
-        if (redirect.isNotEmpty()) {
-            router.redirectToDeepLink(redirect)
-        } else {
-            router.navigateToAuthenticated()
-        }
+fun ConsentsData.updateReviewStatus(consentType: String): ConsentsData = copy(
+    groupedConsents = groupedConsents.mapValues { (_, consents) ->
+        consents.mapNotNull { it.updateReviewStatusIfMatch(consentType) }
     }
+)
 
-    fun onConsentDocumentReviewed(consentType: String) {
-        consentUiState.update { previousState ->
-            previousState.copy(
-                data = previousState.data?.updateReviewStatus(consentType)
-            )
+fun ConsentData.updateReviewStatusIfMatch(consentType: String): ConsentData =
+    takeIf { it.consentType == consentType }?.copy(isReviewed = true) ?: this
+
+fun ConsentsData.updateConsentsError(): ConsentsData = copy(
+    groupedConsents = groupedConsents.mapValues { (consentType, consents) ->
+        val errorMessage = when (consentType) {
+            EDCA_TYPE -> TODO(reason = "Get it from error presenter")
+            BDSA_TYPE -> TODO(reason = "Get it from error presenter")
+            OTHER_TYPE -> TODO(reason = "Get it from error presenter")
+            else -> null
         }
+        consents.map { it.copy(error = errorMessage) }
     }
-
-    fun onConsentErrorStateChanged() {
-        consentUiState.update { previousState ->
-            previousState.copy(
-                data = previousState.data?.updateConsentsError()
-            )
-        }
-    }
-
-    fun onConsentCheckboxChanged() {
-        consentUiState.update { previousState ->
-            previousState.copy(
-                isCheckboxChecked = !previousState.isCheckboxChecked
-            )
-        }
-    }
-
-    fun onConsentErrorChanged() {
-        consentUiState.update { previousState ->
-            previousState.copy(
-                isConsentError = !previousState.isConsentError
-            )
-        }
-    }
-
-    /** --- Utility Methods --- **/
-
-    private fun hasPendingConsentDocumentReview(): Boolean =
-        consentUiState.value.data?.groupedConsents?.values?.flatten()?.any { !it.isReviewed } ?: false
-
-    fun validateAndSubmit() {
-        val hasPendingConsentDocument = hasPendingConsentDocumentReview()
-
-        if (hasPendingConsentDocument) {
-            onConsentErrorStateChanged()
-        }
-
-        if (!consentUiState.value.isCheckboxChecked) {
-            onConsentErrorChanged()
-        }
-
-        if (!hasPendingConsentDocument && consentUiState.value.isCheckboxChecked) {
-            TODO(reason = "Call update consent function from here to update consents")
-        }
-    }
-}
+)
