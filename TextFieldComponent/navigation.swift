a73ccb815ec0938a5f9bb74ef35061552f64fda5
Duela Davis <dueladavis123@gmail.com>
2025-02-28 16:24:30 -0500

    sd

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,25 +1,6 @@
-private fun getErrorMessage(code: String): String =
-    messageCatalogue.formatErrorForNativeAlert(code)
-
-private fun getConsentDocumentErrorMessage(consentData: ConsentData): String {
-    if (!isConsentValidationFailed || consentData.isReviewed) {
-        return ""
-    }
-
-    return when (consentData.consentType) {
-        BDSA_TYPE -> getErrorMessage(DBSA_NOT_READ)
-        EDCA_TYPE -> getErrorMessage(EDCA_NOT_READ)
-        else -> CONSENT_TEXT_UNAVAILABLE
-    }
+fun ConsentsUiState.getConsentPathByType(consentType: String): String? {
+    return this.data?.groupedConsents?.values
+        ?.flatten() // Convert Map<String, List<ConsentData>> to List<ConsentData>
+        ?.find { it.consentType == consentType } // Find the matching consentType
+        ?.consentPath
 }
-
-private fun getCheckboxConsentErrorMessage(): String {
-    return if (isConsentValidationFailed && isConsentChecked) {
-        getErrorMessage(CHECKBOX_NOT_CHECKED)
-    } else {
-        ""
-    }
-}
-
-// Constants for better readability
-private const val CONSENT_TEXT_UNAVAILABLE = "Consent text not available"
