459cbf14297a5f727148c0fd600c97edf9e46baf
Duela Davis <dueladavis123@gmail.com>
2025-03-02 13:00:27 -0500

    sd

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,27 +1,26 @@
-@ViewBuilder
-private func errorAlertView(message: String, code: String?) -> some View {
-    var attributedString = AttributedString(message)
-    let errorCode = code ?? ""
+import SwiftUI
 
-    // Apply color attributes
-    if let messageRange = attributedString.range(of: message) {
-        attributedString[messageRange].foregroundColor = .primary // Change to your desired color
-    }
-    
-    if !errorCode.isEmpty {
-        var errorAttributedString = AttributedString(" " + errorCode)
-        errorAttributedString.foregroundColor = .red // Change to your desired error color
-        
-        attributedString.append(errorAttributedString)
-    }
+struct ErrorCodeModifier: ViewModifier {
+    let errorCode: String?
+    let errorColor: Color
 
-    HStack(alignment: .top, spacing: BankingTheme.dimens.microSmall) {
-        InlineAlert(
-            statusMessage: attributedString,
-            alertType: .error,
-            mode: .borderless(hasIcon: true)
-        )
+    func body(content: Content) -> some View {
+        HStack(alignment: .firstTextBaseline, spacing: 4) {
+            content
+            if let errorCode = errorCode, !errorCode.isEmpty {
+                Text(" \(errorCode)")
+                    .foregroundColor(errorColor)
+                    .font(.caption) // Adjust font style if needed
+            }
+        }
     }
-    .padding(.top, BankingTheme.dimens.small)
-    .frame(maxWidth: .infinity, alignment: .topLeading)
 }
+
+extension View {
+    func appendErrorCode(_ errorCode: String?, color: Color = .red) -> some View {
+        self.modifier(ErrorCodeModifier(errorCode: errorCode, errorColor: color))
+    }
+}
+
+Text("This is an error message")
+    .appendErrorCode("E101", color: .red)
