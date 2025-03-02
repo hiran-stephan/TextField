4876a6002fdfaee3279ce5e59581a23ccd348fb0
Duela Davis <dueladavis123@gmail.com>
2025-03-02 11:12:50 -0500

    asd

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,80 +1,25 @@
-import SwiftUI
-
 extension String {
-    func attributedWithErrorCode(highlightColor: UIColor, defaultColor: UIColor) -> NSAttributedString {
-        let attributedString = NSMutableAttributedString(string: self, attributes: [
-            .foregroundColor: defaultColor
-        ])
-        
+    func attributedWithErrorCode(highlightColor: Color, defaultColor: Color) -> AttributedString {
+        var attributedString = AttributedString(self)
         let pattern = "\\(\\d{4,}\\)"  // Matches error codes like (0045)
 
         if let regex = try? NSRegularExpression(pattern: pattern, options: []) {
-            let matches = regex.matches(in: self, options: [], range: NSRange(location: 0, length: self.utf16.count))
+            let nsString = self as NSString
+            let matches = regex.matches(in: self, options: [], range: NSRange(location: 0, length: nsString.length))
 
             for match in matches {
-                attributedString.addAttribute(.foregroundColor, value: highlightColor, range: match.range)
+                let nsRange = match.range
+                
+                if let lowerBound = attributedString.index(AttributedString.Index(utf16Offset: nsRange.location, in: attributedString), offsetBy: 0, limitedBy: attributedString.endIndex),
+                   let upperBound = attributedString.index(lowerBound, offsetBy: nsRange.length, limitedBy: attributedString.endIndex) {
+                    
+                    let attributedRange = lowerBound..<upperBound
+                    attributedString[attributedRange].foregroundColor = highlightColor
+                }
             }
         }
 
+        attributedString.foregroundColor = defaultColor
         return attributedString
     }
-}
-
-
-
-
-
-
-public struct InlineAlert: View {
-    let statusMessage: AttributedString
-    let alertType: AlertType
-    let mode: Mode
-    
-    public init(statusMessage: String, alertType: AlertType, mode: Mode = .bordered) {
-        self.statusMessage = statusMessage.attributedWithErrorCode(
-            highlightColor: BankingTheme.colors.error,  // Custom color for the error code
-            defaultColor: BankingTheme.colors.textPrimary // Default text color
-        )
-        self.alertType = alertType
-        self.mode = mode
-    }
-
-    public var body: some View {
-        switch mode {
-        case .bordered:
-            borderedView()
-        case .borderless(let hasIcon):
-            borderLessView(hasIcon)
-        }
-    }
-}
-
-
-
-@ViewBuilder
-private func errorAlertView(message: String) -> some View {
-    HStack(alignment: .top, spacing: BankingTheme.dimens.microSmall) {
-        InlineAlert(
-            statusMessage: message,
-            alertType: .error,
-            mode: .borderless(hasIcon: true)
-        )
-    }
-    .padding(.top, BankingTheme.dimens.small)
-    .frame(maxWidth: .infinity, alignment: .topLeading)
-}
-
-public init(
-    statusMessage: String,
-    alertType: AlertType,
-    mode: Mode = .bordered
-) {
-    self.init(
-        statusMessage: AttributedString(statusMessage).attributedWithErrorCode(
-            highlightColor: BankingTheme.colors.error,
-            defaultColor: BankingTheme.colors.textPrimary
-        ),
-        alertType: alertType,
-        mode: mode
-    )
 }
