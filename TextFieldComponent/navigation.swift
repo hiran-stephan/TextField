20c7d5efc2dd2c7816cc7ff69a401a5a0a6b1cdf
Duela Davis <dueladavis123@gmail.com>
2025-03-02 11:20:51 -0500

    jh

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -28,3 +28,43 @@ let attributedStatusMessage = AttributedString(statusMessage).attributedWithErro
         highlightColor: BankingTheme.colors.error,
         defaultColor: BankingTheme.colors.textPrimary
     )
+
+
+extension InlineAlert {
+    func errorCodeColor(_ color: Color, defaultColor: Color = BankingTheme.colors.textPrimary) -> InlineAlert {
+        var modifiedAlert = self
+        modifiedAlert.statusMessage = statusMessage.attributedWithErrorCode(
+            highlightColor: color,
+            defaultColor: defaultColor
+        )
+        return modifiedAlert
+    }
+}
+
+
+extension String {
+    func attributedWithErrorCode(highlightColor: Color, defaultColor: Color) -> AttributedString {
+        var attributedString = AttributedString(self)
+        let pattern = "\\(\\d{4,}\\)"  // Matches error codes like (0045)
+
+        if let regex = try? NSRegularExpression(pattern: pattern, options: []) {
+            let nsString = self as NSString
+            let matches = regex.matches(in: self, options: [], range: NSRange(location: 0, length: nsString.length))
+
+            for match in matches {
+                let nsRange = match.range
+                
+                if let lowerBound = attributedString.index(AttributedString.Index(utf16Offset: nsRange.location, in: attributedString), offsetBy: 0, limitedBy: attributedString.endIndex),
+                   let upperBound = attributedString.index(lowerBound, offsetBy: nsRange.length, limitedBy: attributedString.endIndex) {
+                    
+                    let attributedRange = lowerBound..<upperBound
+                    attributedString[attributedRange].foregroundColor = highlightColor
+                }
+            }
+        }
+
+        attributedString.foregroundColor = defaultColor
+        return attributedString
+    }
+}
+
