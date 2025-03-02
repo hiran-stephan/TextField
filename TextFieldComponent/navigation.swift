1dd2579368264f967d48617fdacefb2d1c5b7359
Duela Davis <dueladavis123@gmail.com>
2025-03-02 13:21:46 -0500

    sa

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,26 +1,11 @@
-import SwiftUI
-
-struct ErrorCodeModifier: ViewModifier {
-    let errorCode: String?
-    let errorColor: Color
-
-    func body(content: Content) -> some View {
-        HStack(alignment: .firstTextBaseline, spacing: 4) {
-            content
-            if let errorCode = errorCode, !errorCode.isEmpty {
-                Text(" \(errorCode)")
-                    .foregroundColor(errorColor)
-                    .font(.caption) // Adjust font style if needed
-            }
+private func createAttributedString() -> AttributedString {
+        var attributedString = AttributedString(statusMessage)
+        
+        if let errorCode = errorCode, !errorCode.isEmpty {
+            var errorAttributedString = AttributedString(" \(errorCode)")
+            errorAttributedString.foregroundColor = errorColor
+            attributedString += errorAttributedString
         }
-    }
-}
 
-extension View {
-    func appendErrorCode(_ errorCode: String?, color: Color = .red) -> some View {
-        self.modifier(ErrorCodeModifier(errorCode: errorCode, errorColor: color))
+        return attributedString
     }
-}
-
-Text("This is an error message")
-    .appendErrorCode("E101", color: .red)
