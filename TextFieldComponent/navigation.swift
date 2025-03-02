8bf71f1648d5734689dfdb7bfccb9a6478729737
Duela Davis <dueladavis123@gmail.com>
2025-03-02 11:26:36 -0500

    asd

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,3 +1,6 @@
+
+import SwiftUI
+
 extension String {
     func attributedWithErrorCode(highlightColor: Color, defaultColor: Color) -> AttributedString {
         var attributedString = AttributedString(self)
@@ -9,8 +12,9 @@ extension String {
 
             for match in matches {
                 let nsRange = match.range
-                
-                if let lowerBound = attributedString.index(AttributedString.Index(utf16Offset: nsRange.location, in: attributedString), offsetBy: 0, limitedBy: attributedString.endIndex),
+
+                // Convert NSRange to AttributedString.Index
+                if let lowerBound = attributedString.index(AttributedString.Index(utf16Offset: nsRange.location, in: attributedString)),
                    let upperBound = attributedString.index(lowerBound, offsetBy: nsRange.length, limitedBy: attributedString.endIndex) {
                     
                     let attributedRange = lowerBound..<upperBound
@@ -24,47 +28,28 @@ extension String {
     }
 }
 
-let attributedStatusMessage = AttributedString(statusMessage).attributedWithErrorCode(
-        highlightColor: BankingTheme.colors.error,
-        defaultColor: BankingTheme.colors.textPrimary
-    )
 
 
 extension InlineAlert {
     func errorCodeColor(_ color: Color, defaultColor: Color = BankingTheme.colors.textPrimary) -> InlineAlert {
-        var modifiedAlert = self
-        modifiedAlert.statusMessage = statusMessage.attributedWithErrorCode(
-            highlightColor: color,
-            defaultColor: defaultColor
+        return InlineAlert(
+            statusMessage: self.statusMessage.attributedWithErrorCode(
+                highlightColor: color,
+                defaultColor: defaultColor
+            ),
+            alertType: self.alertType,
+            mode: self.mode
         )
-        return modifiedAlert
     }
 }
 
-
-extension String {
-    func attributedWithErrorCode(highlightColor: Color, defaultColor: Color) -> AttributedString {
-        var attributedString = AttributedString(self)
-        let pattern = "\\(\\d{4,}\\)"  // Matches error codes like (0045)
-
-        if let regex = try? NSRegularExpression(pattern: pattern, options: []) {
-            let nsString = self as NSString
-            let matches = regex.matches(in: self, options: [], range: NSRange(location: 0, length: nsString.length))
-
-            for match in matches {
-                let nsRange = match.range
-                
-                if let lowerBound = attributedString.index(AttributedString.Index(utf16Offset: nsRange.location, in: attributedString), offsetBy: 0, limitedBy: attributedString.endIndex),
-                   let upperBound = attributedString.index(lowerBound, offsetBy: nsRange.length, limitedBy: attributedString.endIndex) {
-                    
-                    let attributedRange = lowerBound..<upperBound
-                    attributedString[attributedRange].foregroundColor = highlightColor
-                }
-            }
-        }
-
-        attributedString.foregroundColor = defaultColor
-        return attributedString
+struct ContentView: View {
+    var body: some View {
+        InlineAlert(
+            statusMessage: "An error occurred. Please try again. (0045)",
+            alertType: .error
+        )
+        .errorCodeColor(.red, defaultColor: .black) // Highlights (0045) in red, rest in black
+        .padding()
     }
 }
-
