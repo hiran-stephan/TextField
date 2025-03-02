75a3097c20afdd6598f039dfe1f6f0e14acecf29
Duela Davis <dueladavis123@gmail.com>
2025-03-02 10:58:36 -0500

    asd

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,33 +1,25 @@
 import SwiftUI
 
-import SwiftUI
-
 extension String {
-    func attributedWithErrorCode(highlightColor: Color, defaultColor: Color) -> AttributedString {
-        var attributedString = AttributedString(self)
+    func attributedWithErrorCode(highlightColor: UIColor, defaultColor: UIColor) -> NSAttributedString {
+        let attributedString = NSMutableAttributedString(string: self, attributes: [
+            .foregroundColor: defaultColor
+        ])
+        
         let pattern = "\\(\\d{4,}\\)"  // Matches error codes like (0045)
 
         if let regex = try? NSRegularExpression(pattern: pattern, options: []) {
-            let nsString = self as NSString
-            let matches = regex.matches(in: self, options: [], range: NSRange(location: 0, length: nsString.length))
+            let matches = regex.matches(in: self, options: [], range: NSRange(location: 0, length: self.utf16.count))
 
             for match in matches {
-                let nsRange = match.range
-                
-                // Convert NSRange to AttributedString.Index range
-                if let lowerBound = attributedString.index(AttributedString.Index(utf16Offset: nsRange.location, in: attributedString), offsetBy: 0, limitedBy: attributedString.endIndex),
-                   let upperBound = attributedString.index(lowerBound, offsetBy: nsRange.length, limitedBy: attributedString.endIndex) {
-                    
-                    let attributedRange = lowerBound..<upperBound
-                    attributedString[attributedRange].foregroundColor = highlightColor
-                }
+                attributedString.addAttribute(.foregroundColor, value: highlightColor, range: match.range)
             }
         }
 
-        attributedString.foregroundColor = defaultColor
         return attributedString
     }
 }
+
 
 
 
