df6b32defffa176a5c0c6e63466e8944e0d1867c
Duela Davis <dueladavis123@gmail.com>
2025-03-02 13:23:02 -0500

    asd

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,11 +1,11 @@
 private func createAttributedString() -> AttributedString {
-        var attributedString = AttributedString(statusMessage)
-        
-        if let errorCode = errorCode, !errorCode.isEmpty {
-            var errorAttributedString = AttributedString(" \(errorCode)")
-            errorAttributedString.foregroundColor = errorColor
-            attributedString += errorAttributedString
-        }
-
-        return attributedString
+    var attributedString = AttributedString(statusMessage)
+    
+    if let errorCode = errorCode, !errorCode.isEmpty {
+        var errorAttributedString = AttributedString(" \(errorCode)")
+        errorAttributedString.setAttributes(.init([.foregroundColor: errorColor])) // Set color correctly
+        attributedString.append(errorAttributedString) // Append to main message
     }
+
+    return attributedString
+}
