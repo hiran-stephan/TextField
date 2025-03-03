0b4371de08e04622ffdcc9aadcb6b821f992f60e
Duela Davis <dueladavis123@gmail.com>
2025-03-03 10:38:47 -0500

    asd

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -15,3 +15,12 @@ private func createAttributedString(
     attributedString.append(errorAttributedString)  // Append error code
     return attributedString
 }
+
+
+Hi @Chellappan Pillai Rajendran Pi, AnuRaj,
+
+My test devices were automatically updated to the latest iOS version, and I am now unable to connect them to Xcode for development and testing.
+
+I kindly request a replacement test device and would like to return the current ones.
+
+Looking forward to your response. I appreciate your help!
