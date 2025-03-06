f77e84c9580f0b01e52a670a469ba934a549fd12
Duela Davis <dueladavis123@gmail.com>
2025-03-06 14:01:17 -0500

    asd

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,19 +1,22 @@
-// Mocked consents data
-            val consents = listOf(
-                ConsentData(
-                    consentName = "consent-13.pdf",
-                    consentType = "13",
-                    consentVersion = "1234",
-                    consentPath = "/content/dam/us-public/us-bank-digital-banking-service-agreement.pdf",
-                    isReviewed = false,
-                    error = null
-                ),
-                ConsentData(
-                    consentName = "consent-14.pdf",
-                    consentType = "14",
-                    consentVersion = "1235",
-                    consentPath = "/content/dam/us-public/us-bank-digital-banking-service-agreement.pdf",
-                    isReviewed = false,
-                    error = null
-                )
-            )
+Row(
+    modifier = Modifier.fillMaxWidth(),
+    horizontalArrangement = Arrangement.spacedBy(BankingTheme.dimens.small)
+) {
+    Text(
+        text = stepText,
+        style = BankingTheme.typography.heading.small,
+        color = BankingTheme.colors.textPrimary,
+        modifier = Modifier.semantics {
+            contentDescription = stepAccessibilityText
+        }
+    )
+
+    Text(
+        text = headingText,
+        style = BankingTheme.typography.heading.small,
+        color = BankingTheme.colors.textPrimary,
+        modifier = Modifier.semantics {
+            contentDescription = headingAccessibilityText
+        }
+    )
+}
