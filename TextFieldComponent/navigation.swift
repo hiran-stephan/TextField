7ce9b5e1df6286bb20de3d51c53c983fa0dda87d
Duela Davis <dueladavis123@gmail.com>
2025-04-08 14:46:10 -0400

    sdf

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,11 +1,33 @@
-@State private var didCheckFriendlyId = false
+struct TextFieldViewModelInputFilterModifier: ViewModifier {
+    @Binding var text: String
+    let filter: (String) -> String
+    let action: (() -> Void)?
 
-.task {
-    guard !didCheckFriendlyId else { return }
-
-    if let friendlyId = navigationItem.friendlyId, !friendlyId.isEmpty {
-        loginForm.prefillUserNameWith(friendlyId: friendlyId)
+    func body(content: Content) -> some View {
+        content
+            .onChange(of: text) { newValue in
+                let newFiltered = filter(newValue)
+                if newValue != newFiltered {
+                    text = newFiltered
+                }
+                action?()
+            }
     }
-
-    didCheckFriendlyId = true
 }
+
+
+extension View {
+    func replaceText(
+        _ text: Binding<String>,
+        using filter: @escaping (String) -> String,
+        action: (() -> Void)? = nil
+    ) -> some View {
+        modifier(TextFieldViewModelInputFilterModifier(text: text, filter: filter, action: action))
+    }
+}
+
+    .replaceText($phoneNumber, using: viewModel.filterPhoneNumber(number:), action: {
+        viewModel.onChangePhoneNumber(phoneNumber: phoneNumber)
+    })
+
+
