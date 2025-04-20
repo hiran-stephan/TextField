ff7cc039515b53bed759418835e9fa17abc0b5ce
Duela Davis <dueladavis123@gmail.com>
2025-04-20 12:55:05 -0400

    jyf

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,15 +1,5 @@
-sudo rm -rf /Users/Stephan/Library/Java/JavaVirtualMachines/corretto-22.0.2
-sudo rm -rf /Library/Java/JavaVirtualMachines/jdk-17.jdk
-                                                                            
-                                                                            
-export JAVA_HOME=/Users/Stephan/Library/Java/JavaVirtualMachines/corretto-17.0.12/Contents/Home
-
-
-echo 'export JAVA_HOME=/Users/Stephan/Library/Java/JavaVirtualMachines/corretto-17.0.12/Contents/Home' >> ~/.zshrc
-source ~/.zshrc
-
-/usr/libexec/java_home -V
-
-java -version
-
+private func inputState(for field: RecoverUserIdField, using presenter: RecoverUserIdFieldErrorPresenter) -> InputFieldState {
+    presenter.getError(field: field) != nil ? .error : .default
+}
+inputFieldState: inputState(for: .fieldPhoneNumber, using: recoverUserIdFieldErrorPresenter),
 
