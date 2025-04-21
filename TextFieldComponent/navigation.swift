58a2aabb8a898c146da74a8e2859fb7ad2d37b32
Duela Davis <dueladavis123@gmail.com>
2025-04-21 08:42:18 -0400

    zd

--- TextFieldComponent/navigation.swift
+++ TextFieldComponent/navigation.swift
@@ -1,7 +1,10 @@
-let finalState: InputFieldState = {
-    switch inputFieldState {
-    case .error: return isFocused ? .errorFocused : .error
-    case .default: return isFocused ? .focused : .default
-    default: return inputFieldState
-    }
-}()
+private var finalInputFieldState: InputFieldState {
+      switch inputFieldState {
+      case .error:
+          return isFocused ? .errorFocused : .error
+      case .default:
+          return isFocused ? .focused : .default
+      default:
+          return inputFieldState
+      }
+  }
