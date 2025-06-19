8831a7065a3ad894f41d0a00d9dc7b02725b311c
Duela Davis <dueladavis123@gmail.com>
2025-06-19 19:04:30 -0400

    sodas

--- TextFieldComponent/MockData.swift
+++ TextFieldComponent/MockData.swift
@@ -2,3 +2,14 @@
 
 
 nano ~/.gradle/gradle.properties
+
+private var alertSuccessMessageId: String? {
+    model.state?.showAlertPreferenceUpdateSuccessMessage?.id
+}
+
+.onReceive(Just(alertSuccessMessageId).removeDuplicates()) { id in
+    guard let _ = id else { return }
+    showAlertPreferenceUpdateSuccessMessage = model.state?.showAlertPreferenceUpdateSuccessMessage
+}
+
+
