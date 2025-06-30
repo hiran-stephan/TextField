286ec091071dda70deed628e6191d38c63c3cb0f
Duela Davis <dueladavis123@gmail.com>
2025-06-30 14:28:15 -0400

    sdf

--- TextFieldComponent/MockData.swift
+++ TextFieldComponent/MockData.swift
@@ -1,47 +1,22 @@
-package com.cibc.managealerts.ui.screens.selectaccount.presenters
+@Test
+fun `loadCategoryPreferences success updates state`() = runTest {
+    val categoryId = "testCategory"
+    val expectedData = ManageAlertsTestUtils.getAlertsAccountsPreferenceData(category = categoryId)
 
-import com.cibc.services.remoteresource.data.models.ContentFileAlertsConfig
-import com.cibc.services.remoteresource.data.models.LocalizedText
-import com.cibc.services.remoteresource.data.models.MastheadConfig
-import com.cibc.services.utilities.Locale
-import kotlin.test.BeforeTest
-import kotlin.test.Test
-import kotlin.test.assertEquals
-
-class ManageAlertSelectAccountMastheadPresenterTest {
-
-    private val contentFile = ContentFileAlertsConfig(
-        content = mapOf(),
-        masthead = listOf(
-            mapOf(
-                KEY_SELECT_AN_ACCOUNT_MASTHEAD to MastheadConfig(
-                    title = LocalizedText(en = "Select an account")
-                )
-            )
-        ),
-        categories = listOf()
-    )
-
-    private val locale = Locale(appRegion = "US")
-
-    private lateinit var presenter: ManageAlertSelectAccountMastheadPresenter
-
-    @BeforeTest
-    fun setup() {
-        presenter = ManageAlertSelectAccountMastheadPresenter(
-            contentFile = contentFile,
-            locale = locale
-        )
+    everySuspend {
+        repository.fetchAAlertAccountsConfig(categoryId = categoryId, alertId = "")
+    } returns flow {
+        emit(NetworkResultState.Success(id = "id", data = expectedData))
     }
 
-    @Test
-    fun `getSelectAccountMastheadTitle returns correct title`() {
-        val expected = "Select an account"
-        val actual = presenter.mastheadTitle
-        assertEquals(expected, actual)
+    verify(VerifyMode.Ordered) {
+        selectAccountViewModel.loadAccounts(categoryId = categoryId, alertId = "")
     }
 
-    companion object {
-        private const val KEY_SELECT_AN_ACCOUNT_MASTHEAD = "default_masthead"
+    verifySuspend {
+        repository.fetchAAlertAccountsConfig(categoryId = categoryId, alertId = "")
     }
+
+    val actualResult = selectAccountViewModel.manageAlertSelectAccountUiState.value.alertsAccountsPreferenceData
+    assertEquals(expectedData, actualResult)
 }
