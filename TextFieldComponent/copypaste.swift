f9a01f3266e523a1817e1dbaa31b25eb944eb03b
Duela Davis <dueladavis123@gmail.com>
2025-07-09 22:17:51 -0400

    pdf

--- TextFieldComponent/copypaste.swift
+++ TextFieldComponent/copypaste.swift
@@ -1,17 +1,12 @@
-
-fun String.formatCurrencyForTextField(): String {
-    // Use regex to add commas for thousands
+fun String.formatDecimalPart(): String {
     val parts = this.split(".")
-    val integerPart = parts[0].replace(Regex("(\\d)(?=(\\d{3})+(?!\\d))"), "$1,")
 
     val decimalPart = if (parts.size > 1) {
-        // Use only up to 2 digits for cents
         val cents = parts[1].padEnd(2, '0').take(2)
         ".$cents"
     } else {
-        // If there's no decimal part, default to .00
         ".00"
     }
 
-    return integerPart + decimalPart
+    return parts[0] + decimalPart
 }
