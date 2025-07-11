dc623cf42e55e209651a469bf06368eb4def347f
Duela Davis <dueladavis123@gmail.com>
2025-07-10 21:18:57 -0400

    sd

--- TextFieldComponent/copypaste.swift
+++ TextFieldComponent/copypaste.swift
@@ -1,7 +1,11 @@
 fun ManageAlertsConfigSubscription.isAlertFormVisible(accountNumber: String?): Boolean {
-    val isAnySubscriptionActive = subscriptions
-        ?.filter { it.productData?.productNumber == accountNumber }
-        ?.any { it.active == true } == true
+    val isAnySubscriptionActive = if (accountNumber != null) {
+        subscriptions
+            ?.filter { it.productData?.productNumber == accountNumber }
+            ?.any { it.active == true } == true
+    } else {
+        subscriptions?.any { it.active == true } == true
+    }
 
     return isAnySubscriptionActive || alwaysOn
 }
