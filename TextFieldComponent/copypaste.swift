5a3c40fba9263aacac611fa79b62fc8245e1e76a
Duela Davis <dueladavis123@gmail.com>
2025-07-17 09:02:01 -0400

    sdf

--- TextFieldComponent/copypaste.swift
+++ TextFieldComponent/copypaste.swift
@@ -1,52 +1,17 @@
-class BottomSheetHostingController<Content: View>: UIHostingController<Content> {
-    private let detents: [UISheetPresentationController.Detent]
+private val subCategoryOrder = listOf(
+    "security",
+    "processing",
+    "transfers",
+    "bill_payments",
+    "account_balance",
+    "information"
+)
 
-    init(rootView: Content, detents: [UISheetPresentationController.Detent]) {
-        self.detents = detents
-        super.init(rootView: rootView)
-    }
-
-    @objc required dynamic init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-
-        if let presentation = sheetPresentationController {
-            presentation.detents = detents
-            presentation.prefersGrabberVisible = true
-        }
+fun getOrderedCategoryPreferenceData(): List<Pair<String, List<ManageAlertsConfigSubscription>>> {
+    return subCategoryOrder.mapNotNull { key ->
+        categoryPreferenceData?.get(key)?.let { key to it }
     }
 }
 
-public struct BottomSheetView<Content: View>: UIViewControllerRepresentable {
-    let content: () -> Content
-    let detents: [UISheetPresentationController.Detent]
+let categoryPreferenceDataArr = viewModel.getOrderedCategoryPreferenceData()
 
-    public init(
-        detents: [UISheetPresentationController.Detent] = [.fraction(0.75), .large()],
-        @ViewBuilder content: @escaping () -> Content
-    ) {
-        self.content = content
-        self.detents = detents
-    }
-
-    public func makeUIViewController(context: Context) -> BottomSheetHostingController<Content> {
-        BottomSheetHostingController(rootView: content(), detents: detents)
-    }
-
-    public func updateUIViewController(
-        _ uiViewController: BottomSheetHostingController<Content>,
-        context: Context
-    ) {
-        // No updates needed
-    }
-}
-
-
-    .sheet(isPresented: shouldShowLearnMoreDialog, content: {
-        BottomSheetView(detents: [.fraction(0.75), .large()]) {
-            ChangePasswordLearnMoreScreen(viewModel: viewModel)
-        }
-    })
