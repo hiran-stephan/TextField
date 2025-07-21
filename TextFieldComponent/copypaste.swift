6c4170f26a9b1983b96eb6f054669adbdce0bdb2
Duela Davis <dueladavis123@gmail.com>
2025-07-21 11:32:44 -0400

    pdf

--- TextFieldComponent/copypaste.swift
+++ TextFieldComponent/copypaste.swift
@@ -1,1 +1,58 @@
 
+Work History
+Sr. Mobile Developer – CIBC, Toronto, ON
+August 2024 – Present
+
+Developing the CIBC Online Banking application for U.S. customers using Kotlin Multiplatform (KMP) for shared logic and SwiftUI for iOS UI.
+
+Writing Kotlin code in the shared module to ensure feature consistency across platforms.
+
+Translating High-Level Designs (HLD) and Figma UI designs into well-structured, testable code.
+
+Creating Low-Level Designs (LLD) and writing unit test cases to ensure quality and maintainability.
+
+Performing code reviews and collaborating with cross-functional teams across platforms.
+
+Technical Lead – HARMAN CONNECTED SERVICES, Bangalore, India
+February 2022 – June 2023
+
+Spearheaded iOS development efforts, overseeing code quality, and conducting effective code reviews.
+
+Successfully led the migration of the Objective-C codebase to Swift, significantly improving code readability and maintainability.
+
+Prototyped solutions in React Native, exploring the potential for migrating the project to this technology stack.
+
+Pioneered the adoption of the MVVM architecture, enhancing code structure and maintainability.
+
+Contributed to offline storage implementation using Realm and Core Data.
+
+Conducted performance tuning to optimize the app's responsiveness.
+
+Technical Lead – RapidValue IT Services Pvt. Ltd. (Acquired by ASPIRE SYSTEMS), Kochi, India
+January 2019 – February 2022
+
+Led and supervised iOS and Android teams, ensuring code quality and effective project management in GIT and JIRA.
+
+Collaborated closely with the customer team to expedite development and resolve API-related issues.
+
+Seamlessly integrated third-party SDKs, including Firebase, Twilio, and Stripe.
+
+Implemented RESTful API connections for efficient back-end communication.
+
+Led the implementation of CI/CD pipelines using Fastlane.
+
+iOS Team Lead – ILEAF SOLUTIONS PVT LTD, Kochi, India
+April 2015 – January 2019
+
+Developed iOS applications and mentored junior developers.
+
+Oversaw the architecture, code review, and release of apps.
+
+Wrote Unit Tests and contributed to UI test automation.
+
+iOS Developer – ZERONE CONSULTING PVT LTD, Kochi, India
+November 2012 – April 2015
+
+Contributed to the development of multiple iOS applications.
+
+Gained hands-on experience with Objective-C, Xcode, and native development best practices.
