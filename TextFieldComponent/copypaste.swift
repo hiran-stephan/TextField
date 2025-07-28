fa4b7b9e1f7fae03e305edc004c96e59f9f20c60
Duela Davis <dueladavis123@gmail.com>
2025-07-28 09:54:55 -0400

    sec

--- TextFieldComponent/copypaste.swift
+++ TextFieldComponent/copypaste.swift
@@ -1,8 +1,12 @@
-
 Hi,
 
-I'm currently out of the office on sick leave and will not be checking emails. I’ll respond as soon as I’m able.
+Thank you for your message.
 
-If your request is urgent, please contact [Alternate Contact Name] at [email address or phone number].
+I am currently in Canada on a Spouse Open Work Permit (SOWP), which is set to expire in September. My spouse is on a Study Permit and has recently applied for a Post-Graduation Work Permit (PGWP); we are currently awaiting the decision.
 
-Thank you for your understanding.
+I will be submitting my SOWP extension application in the first or second week of August, based on her PGWP approval timeline.
+
+Please let me know if you need any additional information.
+
+Best regards,
+Hiran Stephan
