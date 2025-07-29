b53cc0a074bc3c7df6ce61b8db7cae8e198c3830
Duela Davis <dueladavis123@gmail.com>
2025-07-29 18:56:37 -0400

    sdf

--- TextFieldComponent/copypaste.swift
+++ TextFieldComponent/copypaste.swift
@@ -1,2 +1,16 @@
-Vision Professional Talent Services Inc.
+@State private var showInfoDialog = false
 
+...
+
+.onReceive(model.$state.map(\.shouldShowInfoDialog).removeDuplicates()) { shouldShow in
+    showInfoDialog = shouldShow
+}
+.alert(isPresented: $showInfoDialog) {
+    Alert(
+        title: Text("Info"),
+        message: Text("Your message here"),
+        dismissButton: .default(Text("OK"), action: {
+            model.onInfoCloseButtonClicked()
+        })
+    )
+}
