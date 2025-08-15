e1c2fdde84a24f116c4c1b577415132e3f5f64fd
hiran-stephan <148594893+hiran-stephan@users.noreply.github.com>
2025-08-15 10:03:19 -0400

    Update copypaste.swift

--- TextFieldComponent/copypaste.swift
+++ TextFieldComponent/copypaste.swift
@@ -1,9 +1,6 @@
-else {
-    if let targetIndex = navigator.path.firstIndex(where: { $0.route() == item?.route() }) {
-        // Remove everything after targetIndex
-        navigator.path.removeLast(navigator.path.count - targetIndex - 1)
-    } else {
-        // If not found, just remove last as before
-        navigator.path.removeLast()
-    }
+if let firstAuthIndex = navigator.path.firstIndex(where: { item in
+    item.domain() == NavigationItems.shared.authentication.DOMAIN_SIGNON
+}) {
+    // Remove all items after the firstAuthIndex
+    navigator.path.removeSubrange((firstAuthIndex + 1)..<navigator.path.count)
 }
