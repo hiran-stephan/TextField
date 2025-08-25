9c926adb3f633eed8057ec3bab4cec595801463f
Duela Davis <dueladavis123@gmail.com>
2025-08-25 18:54:36 -0400

    pdf

--- TextFieldComponent/copypaste.swift
+++ TextFieldComponent/copypaste.swift
@@ -14,3 +14,14 @@ _tabSelected = State(initialValue: viewModel.sharedStateWrapped.value.bottomNavS
     .onChange(of: tabSelected) { newValue in
                 viewModel.onBottomNavTabSelected(domain: newValue)
             }
+
+
+// Use the domain stored in SharedUiState
+    private var tabBinding: Binding<String> {
+        Binding(
+            get: { sharedState.value.bottomNavSelectedDomain },
+            set: { newValue in
+                viewModel.updateBottomNavSelection(domain: newValue)
+            }
+        )
+    }
