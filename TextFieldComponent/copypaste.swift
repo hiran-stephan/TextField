baa754b3dc8d81c89ee314d8a15aafb48be985b2
Duela Davis <dueladavis123@gmail.com>
2025-09-09 18:53:47 -0400

    dfxzdf

--- TextFieldComponent/copypaste.swift
+++ TextFieldComponent/copypaste.swift
@@ -1,2 +1,12 @@
-val REGEX_USERNAME_ALLOWED_CHARACTERS: Regex =
-    Regex("""^[A-Za-z0-9 \-.,~!@#&_:${'$'}^*+'/?=;]+$""")
+// test-only (commonTest or jvmTest)
+object FakeRegexPatternProvider : RegexPatternProvider {
+    override val usernameAllowedCharacters =
+        RegExPatterns.REGEX_USERNAME_ALLOWED_CHARACTERS
+    override val usernameTwoCharTwoNumbers =
+        RegExPatterns.REGEX_USERNAME_AT_LEAST_TWO_LETTERS_AND_TWO_NUMBERS
+    override val usernameNoRestrictedCharacters =
+        RegExPatterns.REGEX_USERNAME_NO_RESTRICTED_CHARACTERS
+
+    // <-- mock email so tests don't need platform actuals
+    override val emailSimple: Regex = Regex(".*")
+}
