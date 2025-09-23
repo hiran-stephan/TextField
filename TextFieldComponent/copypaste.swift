d9cd00ac0cb1bba9d6dc0328ca3e9161f1759b08
Duela Davis <dueladavis123@gmail.com>
2025-09-23 12:24:05 -0400

    sad

--- TextFieldComponent/copypaste.swift
+++ TextFieldComponent/copypaste.swift
@@ -1,3 +1,6 @@
 www.linkedin.com/in/dueladavis
 
 
+I would like to refer my spouse, [Spouse’s Full Name], for the Quality Engineer – IT role at CIBC. They have strong experience in software quality assurance, test automation, and defect management, and I believe they would be an excellent fit for the team.
+
+Attached is their resume for your review.
