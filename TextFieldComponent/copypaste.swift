f15a82f68ad74409ba0829cbf5310a45d437ea6d
Duela Davis <dueladavis123@gmail.com>
2025-09-24 19:13:31 -0400

    sd

--- TextFieldComponent/copypaste.swift
+++ TextFieldComponent/copypaste.swift
@@ -1,13 +1,24 @@
-nano ~/.zshrc
-
-export PATH=$PATH:$HOME/Library/Android/sdk/platform-tools
-
-
-source ~/.zshrc
-
-adb devices
-
-
-adb shell settings put global http_proxy 10.0.0.142:9090
-
-adb shell settings put global http_proxy :0
+override fun fetchConsents(): Flow<NetworkResultState<List<ConsentData>>> = flow {
+    emit(NetworkResultState.Loading)
+    val mockResponse = listOf(
+        ConsentData(
+            consentName = "Digital Banking Service Agreement",
+            consentType = "14",
+            consentVersion = "November 2024",
+            consentPath = "/content/dam/us-public-assets/documents/legal/notice/cibc-bank-usa-digital-services-agreement.pdf"
+        ),
+        ConsentData(
+            consentName = "Electronic Disclosure Consent Agreement",
+            consentType = "13",
+            consentVersion = "December 2023",
+            consentPath = "/content/dam/us-public-assets/documents/legal/notice/electronic-disclosure-consent-agreement.pdf"
+        ),
+        ConsentData(
+            consentName = "Electronic Documents Agreement and Disclosure",
+            consentType = "19",
+            consentVersion = "December 2023",
+            consentPath = "/content/dam/us-public-assets/documents/legal/notice/cibc-bank-usa-electronic-documents-agreement-disclosure.pdf"
+        )
+    )
+    emit(NetworkResultState.Success(mockResponse))
+}
