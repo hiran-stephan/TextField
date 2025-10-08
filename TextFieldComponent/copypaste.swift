a503835766e56a31bbc497744e7e9594fb076f22
Duela Davis <dueladavis123@gmail.com>
2025-10-08 15:25:00 -0400

    pdf

--- TextFieldComponent/copypaste.swift
+++ TextFieldComponent/copypaste.swift
@@ -1,12 +1,20 @@
 <table>
   <tr>
-    <td align="center">
-      <strong>Android</strong><br>
-      <video src="https://user-images.githubusercontent.com/xxx/android.mp4" width="300" controls muted playsinline></video>
-    </td>
-    <td align="center">
+    <td align="center" width="50%">
       <strong>iOS</strong><br>
-      <video src="https://user-images.githubusercontent.com/xxx/ios.mp4" width="300" controls muted playsinline></video>
+      <video
+        src="https://cbsc.../b1a5ba4d-fe64-4bdc-8c99-67f9665166bb"
+        width="320" height="700"
+        controls muted playsinline>
+      </video>
+    </td>
+    <td align="center" width="50%">
+      <strong>Android</strong><br>
+      <video
+        src="https://cbsc.../27996fbc-095f-4c22-977b-3a589217e7a00"
+        width="320" height="700"
+        controls muted playsinline>
+      </video>
     </td>
   </tr>
 </table>
