9351179defbc4ef81a74bdcff1c4cb0044716f7c
Duela Davis <dueladavis123@gmail.com>
2025-10-08 15:14:23 -0400

    adds

--- TextFieldComponent/copypaste.swift
+++ TextFieldComponent/copypaste.swift
@@ -1,43 +1,12 @@
-private var tabSelected: Binding<String> {
-       Binding(
-           get: {
-               // KMP current tab
-               sharedState.state?.bottomNavSelectedDomain
-               ?? HomeNavigationItems.Main.shared.domain()
-           },
-           set: { newDomain in
-               guard let item = getNavigationItemFromDomain(domain: newDomain) else { return }
-               // Mirrors your old write to @State, but to KMP
-               viewModel.onBottomNavigationItemChanged(selectedItem: item)
-           }
-       )
-   }
-
-   private var previousTabSelected: Binding<String> {
-       Binding(
-           get: {
-               // KMP previous tab
-               sharedState.state?.previousBottomNavSelectedDomain
-               ?? HomeNavigationItems.Main.shared.domain()
-           },
-           set: { newDomain in
-               guard let item = getNavigationItemFromDomain(domain: newDomain) else { return }
-               // New KMP method you’ll add
-               viewModel.onPreviousBottomNavigationItemChanged(selectedItem: item)
-           }
-       )
-   }
-
-   .onChange(of: tabSelected.wrappedValue) { newValue in
-               // == your existing logic, just reading/writing through the bindings ==
-               guard let navigationItem = getNavigationItemFromDomain(domain: newValue) else { return }
-
-               if tabSelected.wrappedValue != previousTabSelected.wrappedValue {
-                   viewModel.onBottomNavigationItemClick(selectedItem: navigationItem)
-                   previousTabSelected.wrappedValue = newValue
-               } else {
-                   viewModel.navigationToRDCForiOS(navigationItem: navigationItem)
-               }
-           }
-
-   .transaction { $0.disablesAnimations = true } // optional: eliminate micro flicker
+<table>
+  <tr>
+    <td align="center">
+      <strong>Android</strong><br>
+      <video src="https://user-images.githubusercontent.com/xxx/android.mp4" width="300" controls muted playsinline></video>
+    </td>
+    <td align="center">
+      <strong>iOS</strong><br>
+      <video src="https://user-images.githubusercontent.com/xxx/ios.mp4" width="300" controls muted playsinline></video>
+    </td>
+  </tr>
+</table>
