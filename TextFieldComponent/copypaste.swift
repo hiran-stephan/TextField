b1f090c907a1722a3a7273184e4a0fb1dc444383
Duela Davis <dueladavis123@gmail.com>
2025-10-24 12:13:28 -0400

    pdf

--- TextFieldComponent/copypaste.swift
+++ TextFieldComponent/copypaste.swift
@@ -10,4 +10,24 @@ xcrun simctl list
 
 xcrun simctl push booted com.cibc.enterprise.CIBC /Users/Stephan/Documents/Payload.apns
 
+--------------------------------------
+
+ls -1 /Applications | grep -i xcode
+
+# If it's the normal App Store Xcode:
+sudo xcode-select -s "/Applications/Xcode.app/Contents/Developer"
+
+# If it’s a differently named app, adjust the path, e.g.:
+# sudo xcode-select -s "/Applications/Xcode 16.app/Contents/Developer"
+# or
+# sudo xcode-select -s "/Applications/Xcode-beta.app/Contents/Developer"
+
+
+xcode-select -p
+xcrun -f simctl
+xcrun simctl list
+
+
+xcrun simctl push booted com.cibc.enterprise.CIBC /Users/Stephan/Documents/Payload.apns
+
 
