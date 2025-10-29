7caa34bf88517976925e08b713f56fe5c247a57e
Duela Davis <dueladavis123@gmail.com>
2025-10-29 19:43:23 -0400

    asdf

--- TextFieldComponent/copypaste.swift
+++ TextFieldComponent/copypaste.swift
@@ -5,3 +5,12 @@ enum NavigationOption {
     case present
 }
 #endif
+
+it("embeds the FRM screen in a UINavigationController") {
+    let center = PanelHelper.getPanelFrame()?.center
+    expect(center).to(beAKindOf(UINavigationController.self))
+
+    let nav = center as? UINavigationController
+    expect(nav?.visibleViewController).to(beAKindOf(FRMWebViewNavigation.self))
+    // or PushPayLoadViewController.self based on the path you’re testing
+}
