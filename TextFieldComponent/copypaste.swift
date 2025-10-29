ec114c586da04027fa05f28b825f0dc0a730c471
Duela Davis <dueladavis123@gmail.com>
2025-10-29 19:44:48 -0400

    pdf

--- TextFieldComponent/copypaste.swift
+++ TextFieldComponent/copypaste.swift
@@ -14,3 +14,12 @@ it("embeds the FRM screen in a UINavigationController") {
     expect(nav?.visibleViewController).to(beAKindOf(FRMWebViewNavigation.self))
     // or PushPayLoadViewController.self based on the path you’re testing
 }
+
+
+waitUntil(timeout: .seconds(2)) { done in
+    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+        let visible = visibleContent(of: PanelHelper.getPanelFrame()?.center)
+        expect(visible).to(beAKindOf(FRMWebViewNavigation.self)) // or PushPayLoadViewController
+        done()
+    }
+}
