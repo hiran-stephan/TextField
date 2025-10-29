40fd86075b2ef16de5311688b2adf1d43476eb9f
Duela Davis <dueladavis123@gmail.com>
2025-10-29 09:32:53 -0400

    asd

--- TextFieldComponent/copypaste.swift
+++ TextFieldComponent/copypaste.swift
@@ -1,3 +1,3 @@
-git credential-osxkeychain erase
-host=<your git host, e.g., cbcsc...cibcwm.com>
-protocol=https
+git remote -v
+git remote get-url --push origin
+GIT_TRACE=1 GIT_CURL_VERBOSE=1 git push origin HEAD
