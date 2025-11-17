cd6ac6326e74a3bed0ecb0af4986ceec89c86c23
Duela Davis <dueladavis123@gmail.com>
2025-11-17 18:23:41 -0500

    asd

--- TextFieldComponent/copypaste.swift
+++ TextFieldComponent/copypaste.swift
@@ -1,21 +1,54 @@
-func test_performPostSignOnConcurrentFlow_FraudCaseReviewMobileOnly_SetsActionResponseTrue() {
-    let service = SignInService()
-    let actionItemFlag = ActionItemRequiredFlagResponseDto(response: [
-        "fraudCaseReviewMobileOnlyRequired": true,
-        "fraudCaseReviewRequired": false
-    ])
-    BKServiceCache.shared.setCachedActionItemRequiredFlag(actionItemFlag)
-    BKAppPipeline.lineOfBusiness = .cibc
-    FeatureHelper.hasFraudReviewEnabled = true
+func test_performPostSignOn_FraudCaseReviewRequired_setsActionResponseTrue() {
+    let mockOTVC = MockOTVCService()
+    mockOTVC.getRegisteredDeviceSuccess = true
+    mockOTVC.getRegisteredDeviceResponseDto = RegisteredDeviceResponseDto(status: .devicePushEnabled)
 
-    // mock keychain & OTVC service to:
-    //  - return a deviceTag
-    //  - return status = .pushEnabled
+    let service = SignInService()
+    service.otvcService = mockOTVC
+
+    let actionItemFlag = ActionItemRequiredFlagResponseDto(response: [
+        "fraudCaseReviewRequired": true,
+        "fraudCaseReviewMobileOnlyRequired": false
+    ])
+
+    BKServiceCache.shared.setCachedActionItemRequiredFlag(actionItemFlag)
+    FeatureHelper.setFraudReviewEnabled(true)
 
     let expectation = self.expectation(description: "Completion")
-    service.performPostSignOnConcurrentFlow { (_, _, _, _, actionResponse) in
+
+    service.performPostSignOnConcurrentFlow { (_,_,_,_,_,actionResponse) in
         XCTAssertTrue(actionResponse.value == true)
         expectation.fulfill()
     }
+
     waitForExpectations(timeout: 5)
 }
+
+
+
+func test_performPostSignOn_FraudCaseReviewMobileOnly_setsActionResponseTrue() {
+    let mockOTVC = MockOTVCService()
+    mockOTVC.getRegisteredDeviceSuccess = true
+    mockOTVC.getRegisteredDeviceResponseDto = RegisteredDeviceResponseDto(status: .devicePushEnabled)
+
+    let service = SignInService()
+    service.otvcService = mockOTVC
+
+    let actionItemFlag = ActionItemRequiredFlagResponseDto(response: [
+        "fraudCaseReviewRequired": false,
+        "fraudCaseReviewMobileOnlyRequired": true
+    ])
+
+    BKServiceCache.shared.setCachedActionItemRequiredFlag(actionItemFlag)
+    FeatureHelper.setFraudReviewEnabled(true)
+
+    let expectation = self.expectation(description: "Completion")
+
+    service.performPostSignOnConcurrentFlow { (_,_,_,_,_,actionResponse) in
+        XCTAssertTrue(actionResponse.value == true)
+        expectation.fulfill()
+    }
+
+    waitForExpectations(timeout: 5)
+}
+
